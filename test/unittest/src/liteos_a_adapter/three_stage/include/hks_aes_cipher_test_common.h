b670bda765f4eca814079ca57b2e03c39d21836b
chen_xin_tao <chenxintao2@huawei.com>
2022-03-25 19:25:15 -0700

    The namespace is not commented out.
    Signed-off-by: chen_xin_tao <chenxintao2@huawei.com>

--- test/unittest/src/liteos_a_adapter/three_stage/include/hks_aes_cipher_test_common.h
+++ test/unittest/src/liteos_a_adapter/three_stage/include/hks_aes_cipher_test_common.h
@@ -40,5 +40,5 @@ int32_t HksAESCipherTestCaseCcm(const struct HksBlob *keyAlias, struct HksParamS
     struct HksParamSet *encryptParamSet, struct HksParamSet *decryptParamSet);
 int32_t HksAesCipherTestCaseGcm(const struct HksBlob *keyAlias, struct HksParamSet *genParamSet,
     struct HksParamSet *encryptParamSet, struct HksParamSet *decryptParamSet);
-} // namespace Unittest::AesCipher 
+} // namespace Unittest::AesCipher
 #endif // HKS_AES_CIPHER_TEST_COMMON_H
