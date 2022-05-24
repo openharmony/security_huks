247c5f0ca46ff1036d3b5d5e83965fc56840f95a
guo-longfei20 <guolongfei10@huawei.com>
2022-05-24 00:03:20 -0700

    dtfuzz用例添加
    Signed-off-by: guo-longfei20 <guolongfei10@huawei.com>

---
+++ test/fuzz_test/hksattestkey_fuzzer/hksattestkey_fuzzer.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hksattestkey_fuzzer.h"
+
+#include "hks_api.h"
+#include "hks_mem.h"
+#include "hks_param.h"
+#include "hks_type.h"
+
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+#include <securec.h>
+
+namespace OHOS
+{
+    bool DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
+    {
+        if (data == nullptr || size <= (10 + sizeof(struct HksParamSet) + sizeof(struct HksCertChain))) {
+            return false;
+        }
+
+        uint8_t *mydata = (uint8_t *)malloc(sizeof(uint8_t) * size);
+        if (mydata == nullptr)
+        {
+            return false;
+        }
+
+        (void)memcpy_s(mydata, size, data, size);
+
+        struct HksBlob keyAlias = { 10, (uint8_t *)mydata };
+
+        struct HksCertChain *certChain = (struct HksCertChain *)(mydata + 10);
+        certChain->certsCount = 0;
+        certChain->certs = nullptr;
+
+        struct HksParamSet *paramSet = (struct HksParamSet *)(mydata + sizeof(struct HksCertChain) + 10);
+        int paramSize = size - 10 - sizeof(struct HksCertChain);
+        paramSet->paramSetSize = paramSize < HKS_PARAM_SET_MAX_SIZE ? paramSize : HKS_PARAM_SET_MAX_SIZE;
+
+        (void)HksAttestKey(&keyAlias, paramSet, certChain);
+
+        if(mydata != nullptr)
+        {
+            free(mydata);
+        }
+
+        return true;
+    }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
---
+++ test/fuzz_test/hksupdate_fuzzer/hksupdate_fuzzer.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hksupdate_fuzzer.h"
+
+#include "hks_api.h"
+#include "hks_mem.h"
+#include "hks_param.h"
+#include "hks_type.h"
+
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+#include <securec.h>
+
+namespace OHOS
+{
+    bool DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
+    {
+        if (data == nullptr || size <= (sizeof(struct HksParamSet) + 30))
+        {
+            return false;
+        }
+
+        uint8_t *mydata = (uint8_t *)malloc(sizeof(uint8_t) * size);
+        if (mydata == nullptr)
+        {
+            return false;
+        }
+
+        (void)memcpy_s(mydata, size, data, size);
+
+        struct HksBlob handle = { 10, (uint8_t *)mydata };
+        struct HksBlob inData = { 10, (uint8_t *)(mydata + 10) };
+        struct HksBlob outData = { 10, (uint8_t *)(mydata + 20) };
+
+        struct HksParamSet *paramSet = (struct HksParamSet *)(mydata + 30);
+        paramSet->paramSetSize = (size - 20) < HKS_PARAM_SET_MAX_SIZE ? (size - 20) : HKS_PARAM_SET_MAX_SIZE;
+
+        HksUpdate(&handle, paramSet, &inData, &outData);
+
+        if(mydata != nullptr) {
+            free(mydata);
+        }
+
+        return true;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
