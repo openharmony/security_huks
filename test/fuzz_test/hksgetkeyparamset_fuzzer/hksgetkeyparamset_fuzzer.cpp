3e89323b1f18e52a15d000575bd790369e9401d9
guo-longfei20 <guolongfei10@huawei.com>
2022-05-24 00:03:20 -0700

    dtfuzz用例添加
    Signed-off-by: guo-longfei20 <guolongfei10@huawei.com>

---
+++ test/fuzz_test/hksgetkeyparamset_fuzzer/hksgetkeyparamset_fuzzer.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hksgetkeyparamset_fuzzer.h"
+
+#include "hks_api.h"
+#include "hks_mem.h"
+#include "hks_param.h"
+#include "hks_type.h"
+
+#include <stddef.h>
+#include <stdint.h>
+#include <securec.h>
+
+namespace OHOS {
+    bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+    {
+        if (data == nullptr || size <= (sizeof(struct HksParamSet) * 2 + 10))
+        {
+            return false;
+        }
+
+        uint8_t *mydata = (uint8_t *)malloc(sizeof(uint8_t) * size);
+        if(mydata == nullptr) {
+            return false;
+        }
+
+        (void)memcpy_s(mydata, size, data, size);
+
+        struct HksBlob keyAlias = { 10, (uint8_t *)mydata };
+
+        int paramSize = (size - 10) / 2;
+        struct HksParamSet *paramSetIn = (struct HksParamSet *)(mydata + 10);
+        paramSetIn->paramSetSize = paramSize < HKS_PARAM_SET_MAX_SIZE ? paramSize : HKS_PARAM_SET_MAX_SIZE;
+
+        struct HksParamSet *paramSetOut = (struct HksParamSet *)(mydata + 10 + paramSize);
+        paramSetOut->paramSetSize = paramSize < HKS_PARAM_SET_MAX_SIZE ? paramSize : HKS_PARAM_SET_MAX_SIZE;
+
+        (void)HksGetKeyParamSet(&keyAlias, paramSetIn, paramSetOut);
+
+        if(mydata != nullptr) {
+            free(mydata);
+        }
+
+        return true;
+    }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
+
