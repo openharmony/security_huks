d97bd4d5d43a6f5b2c0a36482c587b4ff62f8446
guo-longfei20 <guolongfei10@huawei.com>
2022-05-25 01:22:24 -0700

    头文件顺序修改
    Signed-off-by: guo-longfei20 <guolongfei10@huawei.com>

--- test/fuzz_test/hksgetsdkversion_fuzzer/hksgetsdkversion_fuzzer.cpp
+++ test/fuzz_test/hksgetsdkversion_fuzzer/hksgetsdkversion_fuzzer.cpp
@@ -14,12 +14,12 @@
  */
 #include "hksgetsdkversion_fuzzer.h"
 
+#include <securec.h>
+
 #include "hks_api.h"
 #include "hks_mem.h"
 #include "hks_param.h"
 #include "hks_type.h"
-
-#include <securec.h>
 
 namespace OHOS {
     bool DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
