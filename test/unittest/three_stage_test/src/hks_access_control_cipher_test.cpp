808c9b6f8e033d9e90800019c1b470be5aeaf878
wKyrong <wangkrhust@163.com>
2022-07-31 17:24:54 +0800

    add access controll test for agree and cipher
    Signed-off-by: wKyrong <wangkrhust@163.com>

---
+++ test/unittest/three_stage_test/src/hks_access_control_cipher_test.cpp
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hks_access_control_cipher_test.h"
+#include "hks_access_control_test_common.h"
+#include "hks_api.h"
+
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace testing::ext;
+namespace Unittest::HksAccessControlPartTest {
+class HksAccessControlCipherTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+
+    static void TearDownTestCase(void);
+
+    void SetUp();
+
+    void TearDown();
+};
+
+void HksAccessControlCipherTest::SetUpTestCase(void)
+{
+}
+
+void HksAccessControlCipherTest::TearDownTestCase(void)
+{
+}
+
+void HksAccessControlCipherTest::SetUp()
+{
+    EXPECT_EQ(HksInitialize(), 0);
+}
+
+void HksAccessControlCipherTest::TearDown()
+{
+}
+
+/* 001: gen aes for cipher; init for cipher */
+const TestAccessCaseParams HKS_ACCESS_TEST_001_PARAMS = {
+    .genParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_AES },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_ENCRYPT | HKS_KEY_PURPOSE_DECRYPT },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_AES_KEY_SIZE_128 },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_NONE },
+            { .tag = HKS_TAG_BLOCK_MODE, .uint32Param = HKS_MODE_CBC },
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_PIN },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_NORMAL },
+        },
+    .initParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_AES },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_ENCRYPT },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_AES_KEY_SIZE_128 },
+            { .tag = HKS_TAG_DIGEST, .uint32Param = HKS_DIGEST_NONE },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_NONE },
+            { .tag = HKS_TAG_BLOCK_MODE, .uint32Param = HKS_MODE_CBC },
+            { .tag = HKS_TAG_IV, .blob = { .size = IV_SIZE, .data = (uint8_t *)IV }},
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_PIN },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_NORMAL },
+        },
+    .initResult = HKS_SUCCESS
+};
+
+/* 002: gen sm4 for cipher; init for cipher */
+const TestAccessCaseParams HKS_ACCESS_TEST_002_PARAMS = {
+    .genParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_SM4 },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_ENCRYPT | HKS_KEY_PURPOSE_DECRYPT },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_SM4_KEY_SIZE_128 },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_PKCS7 },
+            { .tag = HKS_TAG_BLOCK_MODE, .uint32Param = HKS_MODE_CBC },
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_PIN },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_NORMAL },
+        },
+    .initParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_SM4 },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_ENCRYPT },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_SM4_KEY_SIZE_128 },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_PKCS7 },
+            { .tag = HKS_TAG_BLOCK_MODE, .uint32Param = HKS_MODE_CBC },
+            { .tag = HKS_TAG_IV, .blob = { .size = IV_SIZE, .data = (uint8_t *)IV }},
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_PIN },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_CUSTOM },
+        },
+    .initResult = HKS_SUCCESS
+};
+
+/**
+ * @tc.name: HksCheckAuthTest.HksCheckPurposeTest001
+ * @tc.desc: alg-AES gen-pur-Encrypt.
+ * @tc.type: FUNC
+ * @tc.result:HKS_SUCCESS
+ */
+HWTEST_F(HksAccessControlCipherTest, HksAccessCipherPartTest001, TestSize.Level0)
+{
+    HKS_LOG_I("Enter HksAccessCipherPartTest001");
+    uint64_t secureUid = 1;
+    uint64_t enrolledId = 2;
+    uint64_t time = 0;
+    EXPECT_EQ(CheckAccessCipherTest(HKS_ACCESS_TEST_001_PARAMS, secureUid, enrolledId, time), HKS_SUCCESS);
+}
+/**
+ * @tc.name: HksCheckAuthTest.HksCheckPurposeTest001
+ * @tc.desc: alg-AES gen-pur-Encrypt.
+ * @tc.type: FUNC
+ * @tc.result:HKS_SUCCESS
+ */
+HWTEST_F(HksAccessControlCipherTest, HksAccessCipherPartTest002, TestSize.Level0)
+{
+    HKS_LOG_I("Enter HksAccessCipherPartTest002");
+    uint64_t secureUid = 1;
+    uint64_t enrolledId = 1;
+    uint64_t time = 0;
+    EXPECT_EQ(CheckAccessCipherTest(HKS_ACCESS_TEST_002_PARAMS, secureUid, enrolledId, time), HKS_SUCCESS);
+}
+}
