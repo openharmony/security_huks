3885596202fdc366a7a3fd8a3d04e12e6f051844
zhangcheng <zhangcheng195@huawei.com>
2022-08-02 20:04:42 +0800

    修改编码规范 for 378dc22398748b5236be45638e817beaeac4f544
    Signed-off-by: zhangcheng <zhangcheng195@huawei.com>

--- test/unittest/three_stage_test/src/hks_access_control_derive_test.cpp
+++ test/unittest/three_stage_test/src/hks_access_control_derive_test.cpp
@@ -110,7 +110,7 @@ const TestAccessCaseParams HKS_ACCESS_TEST_002_PARAMS = {
             { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_FACE },
             { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_NEW_BIO_ENROLL },
             { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_CUSTOM },
-			{ .tag = HKS_TAG_CHALLENGE_POS, .uint32Param = 0 },
+            { .tag = HKS_TAG_CHALLENGE_POS, .uint32Param = 0 },
         },
     .initParams =
         {
@@ -124,7 +124,7 @@ const TestAccessCaseParams HKS_ACCESS_TEST_002_PARAMS = {
             { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_FACE },
             { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_NEW_BIO_ENROLL },
             { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_CUSTOM },
-			{ .tag = HKS_TAG_CHALLENGE_POS, .uint32Param = 0 },
+            { .tag = HKS_TAG_CHALLENGE_POS, .uint32Param = 0 },
         },
     .initResult = HKS_SUCCESS
 };
