dca76a475aee214584e04980604aa6d550b1c58d
CheungVane <zhangwenzhi3@huawei.com>
2022-08-11 15:31:04 +0800

    fix access control bug and add tdd Signed-off-by: zhangwenzhi <zhangwenzhi3@huawei.com>

--- test/unittest/huks_standard_test/three_stage_test/src/hks_access_control_rsa_sign_verify_test.cpp
+++ test/unittest/huks_standard_test/three_stage_test/src/hks_access_control_rsa_sign_verify_test.cpp
@@ -346,8 +346,50 @@ const TestAccessCaseRSAParams HKS_ACCESS_TEST_RSA_SIGN_006_PARAMS = {
     .initResult = HKS_SUCCESS
 };
 
+/*
+mock:
+secureUid = 1
+enrollinfo: authtype = FINGERPRINT, enrolledId = 2
+*/
+const TestAccessCaseRSAParams HKS_ACCESS_TEST_RSA_SIGN_007_PARAMS = {
+    .genParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_RSA },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_SIGN | HKS_KEY_PURPOSE_VERIFY },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_RSA_KEY_SIZE_512 },
+            { .tag = HKS_TAG_DIGEST, .uint32Param = HKS_DIGEST_SHA1 },
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_FINGERPRINT },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_PKCS1_V1_5 },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_NORMAL },
+        },
+    .signParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_RSA },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_SIGN },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_RSA_KEY_SIZE_512 },
+            { .tag = HKS_TAG_DIGEST, .uint32Param = HKS_DIGEST_SHA1 },
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_FINGERPRINT },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_PKCS1_V1_5 },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_NORMAL },
+        },
+    .verifyParams =
+        {
+            { .tag = HKS_TAG_ALGORITHM, .uint32Param = HKS_ALG_RSA },
+            { .tag = HKS_TAG_PURPOSE, .uint32Param = HKS_KEY_PURPOSE_VERIFY },
+            { .tag = HKS_TAG_KEY_SIZE, .uint32Param = HKS_RSA_KEY_SIZE_512 },
+            { .tag = HKS_TAG_DIGEST, .uint32Param = HKS_DIGEST_SHA1 },
+            { .tag = HKS_TAG_USER_AUTH_TYPE, .uint32Param = HKS_USER_AUTH_TYPE_FINGERPRINT },
+            { .tag = HKS_TAG_PADDING, .uint32Param = HKS_PADDING_PKCS1_V1_5 },
+            { .tag = HKS_TAG_KEY_AUTH_ACCESS_TYPE, .uint32Param = HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD },
+            { .tag = HKS_TAG_CHALLENGE_TYPE, .uint32Param = HKS_CHALLENGE_TYPE_NORMAL },
+        },
+    .initResult = HKS_SUCCESS
+};
+
 /**
- * @tc.name: HksAccessControlRsaSignVerifyTest.HksAcRsaSignVerifyTest002
+ * @tc.name: HksAccessControlRsaSignVerifyTest.HksAcRsaSignVerifyTest001
  * @tc.desc: pin test.
  * @tc.type: FUNC
  * @tc.result:HKS_SUCCESS
@@ -457,6 +499,25 @@ HWTEST_F(HksAccessControlRsaSignVerifyTest, HksAcRsaSignVerifyTest006, TestSize.
         .authType = 1
     };
     EXPECT_EQ(HksAcRsaSignVerifyTestAbnormalCase(HKS_ACCESS_TEST_RSA_SIGN_006_PARAMS, RSAIDMParams006),
+        HKS_SUCCESS);
+}
+
+/**
+ * @tc.name: HksAccessControlRsaSignVerifyTest.HksAcRsaSignVerifyTest007
+ * @tc.desc: fingerprint test, with access type as HKS_AUTH_ACCESS_INVALID_CLEAR_PASSWORD.
+ * @tc.type: FUNC
+ * @tc.result:HKS_SUCCESS
+ */
+HWTEST_F(HksAccessControlRsaSignVerifyTest, HksAcRsaSignVerifyTest007, TestSize.Level0)
+{
+    HKS_LOG_I("Enter HksAcRsaSignVerifyTest007");
+    const IDMParams RSAIDMParams002 = {
+        .secureUid = 1,
+        .enrolledId = 2,
+        .time = 0,
+        .authType = 4
+    };
+    EXPECT_EQ(HksAcRsaSignVerifyTestNormalCase(HKS_ACCESS_TEST_RSA_SIGN_007_PARAMS, RSAIDMParams002),
         HKS_SUCCESS);
 }
 }
