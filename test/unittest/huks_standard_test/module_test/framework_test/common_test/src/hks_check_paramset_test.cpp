f153425f75b7965b128803a002f576de048dac45
CheungVane <zhangwenzhi3@huawei.com>
2022-08-23 10:31:08 +0800

    将分支测试拆分为独立测试 Signed-off-by: zhangwenzhi <zhangwenzhi3@huawei.com>

--- test/unittest/huks_standard_test/module_test/framework_test/common_test/src/hks_check_paramset_test.cpp
+++ test/unittest/huks_standard_test/module_test/framework_test/common_test/src/hks_check_paramset_test.cpp
@@ -146,56 +146,123 @@ static void HksCoreCheckMacParamsTest(struct HksCoreCheckMacParamsParam *param)
 
 /**
  * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest001
- * @tc.desc: tdd HksCoreCheckMacParams, make tests for all branches
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with nonexist purpose, expecting HKS_ERROR_CHECK_GET_PURPOSE_FAIL
  * @tc.type: FUNC
  */
 HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest001, TestSize.Level0)
 {
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest001");
     struct HksParamSet *paramSet = NULL;
     int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_nonexistTag, g_normalTag, &paramSet);
     EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
     struct HksCoreCheckMacParamsParam param1 = { NULL, paramSet, NULL, NULL, true, HKS_ERROR_CHECK_GET_PURPOSE_FAIL};
     HksCoreCheckMacParamsTest(&param1);
-
     HksFreeParamSet(&paramSet);
-    ret = BuildHksCoreCheckMacParamsTestParamSet(g_invalidTag, g_normalTag, &paramSet);
+}
+
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest002
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with invalid purpose, expecting HKS_ERROR_INVALID_PURPOSE
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest002, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest002");
+    struct HksParamSet *paramSet = NULL;
+    int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_invalidTag, g_normalTag, &paramSet);
     EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
     struct HksCoreCheckMacParamsParam param2 = { NULL, paramSet, NULL, NULL, true, HKS_ERROR_INVALID_PURPOSE};
     HksCoreCheckMacParamsTest(&param2);
-
     HksFreeParamSet(&paramSet);
-    ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_nonexistTag, &paramSet);
+}
+
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest003
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with nonexist digest, expecting HKS_ERROR_CHECK_GET_DIGEST_FAIL
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest003, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest003");
+    struct HksParamSet *paramSet = NULL;
+    int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_nonexistTag, &paramSet);
     EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
     struct HksCoreCheckMacParamsParam param3 = { NULL, paramSet, NULL, NULL, true, HKS_ERROR_CHECK_GET_DIGEST_FAIL};
     HksCoreCheckMacParamsTest(&param3);
-
     HksFreeParamSet(&paramSet);
-    ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_invalidTag, &paramSet);
+}
+
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest004
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with invalid digest, expecting HKS_ERROR_INVALID_DIGEST
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest004, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest004");
+    struct HksParamSet *paramSet = NULL;
+    int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_invalidTag, &paramSet);
     EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
     struct HksCoreCheckMacParamsParam param4 = { NULL, paramSet, NULL, NULL, true, HKS_ERROR_INVALID_DIGEST};
     HksCoreCheckMacParamsTest(&param4);
-
     HksFreeParamSet(&paramSet);
-    ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_normalTag, &paramSet);
-    EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest005
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with too small macBlob, expecting HKS_ERROR_BUFFER_TOO_SMALL
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest005, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest005");
+    struct HksParamSet *paramSet = NULL;
+    int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_normalTag, &paramSet);
+    EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
     uint8_t macData[g_sha256Len - 1];
     struct HksBlob macBlob = { g_sha256Len - 1, macData };
     struct HksCoreCheckMacParamsParam param5 = { NULL, paramSet, NULL, &macBlob, true, HKS_ERROR_BUFFER_TOO_SMALL};
     HksCoreCheckMacParamsTest(&param5);
+    HksFreeParamSet(&paramSet);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest006
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with too small keyBlob, expecting HKS_ERROR_INVALID_KEY_SIZE
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest006, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest006");
+    struct HksParamSet *paramSet = NULL;
+    int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_normalTag, &paramSet);
+    EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
     uint8_t macData2[g_sha256Len];
     struct HksBlob macBlob2 = { g_sha256Len, macData2 };
     uint8_t keyData[g_sha256Len - 1];
     struct HksBlob keyBlob = { g_sha256Len - 1, keyData };
     struct HksCoreCheckMacParamsParam param6 = { &keyBlob, paramSet, NULL, &macBlob2, true, HKS_ERROR_INVALID_KEY_SIZE};
     HksCoreCheckMacParamsTest(&param6);
+    HksFreeParamSet(&paramSet);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest006
+ * @tc.desc: tdd HksCoreCheckMacParams, make test with correct params, expecting HKS_SUCCESS
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest007, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest007");
+    struct HksParamSet *paramSet = NULL;
+    int32_t ret = BuildHksCoreCheckMacParamsTestParamSet(g_normalTag, g_normalTag, &paramSet);
+    EXPECT_EQ(ret, HKS_SUCCESS) << "BuildHksCoreCheckMacParamsTestParamSet failed, ret = " << ret;
+    uint8_t macData2[g_sha256Len];
+    struct HksBlob macBlob2 = { g_sha256Len, macData2 };
     uint8_t keyData2[g_sha256Len];
     struct HksBlob keyBlob2 = { g_sha256Len, keyData2 };
     struct HksCoreCheckMacParamsParam param7 = { &keyBlob2, paramSet, NULL, &macBlob2, true, HKS_SUCCESS};
     HksCoreCheckMacParamsTest(&param7);
-
     HksFreeParamSet(&paramSet);
 }
 
@@ -363,52 +430,152 @@ static void HksCoreCheckDeriveKeyParamsTest(struct HksCoreCheckDeriveKeyParamsTa
 }
 
 /**
- * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest002
- * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make tests for all branches
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest008
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with nonexist alg, expecting HKS_ERROR_CHECK_GET_ALG_FAIL
  * @tc.type: FUNC
  */
-HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest002, TestSize.Level0)
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest008, TestSize.Level0)
 {
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest008");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam1 = { g_nonexistTag, g_normalTag, g_normalTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam1, HKS_ERROR_CHECK_GET_ALG_FAIL);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest009
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with invalid alg, expecting HKS_ERROR_INVALID_ALGORITHM
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest009, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest009");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam2 = { g_invalidTag, g_normalTag, g_normalTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam2, HKS_ERROR_INVALID_ALGORITHM);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest010
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with nonexist purpose, expecting
+ *           HKS_ERROR_CHECK_GET_PURPOSE_FAIL
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest010, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest010");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam3 = { g_normalTag, g_nonexistTag, g_normalTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam3, HKS_ERROR_CHECK_GET_PURPOSE_FAIL);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest011
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with invalid purpose, expecting
+ *           HKS_ERROR_INVALID_PURPOSE
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest011, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest011");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam4 = { g_normalTag, g_invalidTag, g_normalTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam4, HKS_ERROR_INVALID_PURPOSE);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest012
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with nonexist digest, expecting
+ *           HKS_ERROR_CHECK_GET_DIGEST_FAIL
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest012, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest012");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam5 = { g_normalTag, g_normalTag, g_nonexistTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam5, HKS_ERROR_CHECK_GET_DIGEST_FAIL);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest013
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with invalid digest, expecting
+ *           HKS_ERROR_INVALID_DIGEST
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest013, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest013");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam6 = { g_normalTag, g_normalTag, g_invalidTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam6, HKS_ERROR_INVALID_DIGEST);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest014
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with nonexist iteration, expecting
+ *           HKS_ERROR_CHECK_GET_ITERATION_FAIL
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest014, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest014");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam7 = { g_normalTag, g_normalTag, g_normalTag, g_nonexistTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam7, HKS_ERROR_CHECK_GET_ITERATION_FAIL);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest015
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with invalid iteration, expecting
+ *           HKS_ERROR_INVALID_ITERATION
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest015, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest015");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam8 = { g_normalTag, g_normalTag, g_normalTag, g_invalidTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam8, HKS_ERROR_INVALID_ITERATION);
 
+}
+
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest016
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with nonexist salt, expecting
+ *           HKS_ERROR_CHECK_GET_SALT_FAIL
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest016, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest016");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam9 = { g_normalTag, g_normalTag, g_normalTag, g_normalTag,
         g_nonexistTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam9, HKS_ERROR_CHECK_GET_SALT_FAIL);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest017
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with invalid salt, expecting
+ *           HKS_ERROR_INVALID_SALT
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest017, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest017");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam10 = { g_normalTag, g_normalTag, g_normalTag, g_normalTag,
         g_invalidTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam10, HKS_ERROR_INVALID_SALT);
+}
 
+/**
+ * @tc.name: HksFrameworkCommonCheckParamsetTest.HksFrameworkCommonCheckParamsetTest018
+ * @tc.desc: tdd HksCoreCheckDeriveKeyParams, make test with correct params, expecting HKS_SUCCESS
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksFrameworkCommonCheckParamsetTest, HksFrameworkCommonCheckParamsetTest018, TestSize.Level0)
+{
+    HKS_LOG_I("enter HksFrameworkCommonCheckParamsetTest018");
     struct HksCoreCheckDeriveKeyParamsTagParam tagParam11 = { g_normalTag, g_normalTag, g_normalTag, g_normalTag,
         g_normalTag };
     HksCoreCheckDeriveKeyParamsTest(&tagParam11, HKS_SUCCESS);
