7a4695684b3fd8f9ad79d7307371c25ef419bfcf
wKyrong <wangkairong@huawei.com>
2022-10-09 22:18:07 +0800

    add native api compatibility test
    Signed-off-by: wKyrong <wangkairong@huawei.com>

---
+++ test/unittest/huks_standard_test/module_test/interface_test/include/hks_native_api_compatibility_test.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HKS_NATIVE_API_COMPATIBILITY_TEST_H
+#define HKS_NATIVE_API_COMPATIBILITY_TEST_H
+
+namespace Unittest::HksNativeApiCompatibilityTest {
+int HksNativeApiCompatibilityTest001(void);
+int HksNativeApiCompatibilityTest002(void);
+int HksNativeApiCompatibilityTest003(void);
+int HksNativeApiCompatibilityTest004(void);
+int HksNativeApiCompatibilityTest005(void);
+int HksNativeApiCompatibilityTest006(void);
+int HksNativeApiCompatibilityTest007(void);
+int HksNativeApiCompatibilityTest008(void);
+int HksNativeApiCompatibilityTest009(void);
+int HksNativeApiCompatibilityTest010(void);
+int HksNativeApiCompatibilityTest011(void);
+int HksNativeApiCompatibilityTest012(void);
+int HksNativeApiCompatibilityTest013(void);
+int HksNativeApiCompatibilityTest014(void);
+int HksNativeApiCompatibilityTest015(void);
+int HksNativeApiCompatibilityTest016(void);
+int HksNativeApiCompatibilityTest017(void);
+int HksNativeApiCompatibilityTest018(void);
+int HksNativeApiCompatibilityTest019(void);
+int HksNativeApiCompatibilityTest020(void);
+int HksNativeApiCompatibilityTest021(void);
+int HksNativeApiCompatibilityTest022(void);
+int HksNativeApiCompatibilityTest023(void);
+int HksNativeApiCompatibilityTest024(void);
+
+int HksNativeApiCompatibilityTest060(void);
+int HksNativeApiCompatibilityTest061(void);
+}
+#endif
