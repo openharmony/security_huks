cb2f587caac093b86f67e2d41667784e468e92a5
CheungVane <zhangwenzhi3@huawei.com>
2022-11-22 16:34:26 +0800

    将message code归一 Signed-off-by: zhangwenzhi <zhangwenzhi3@huawei.com>

---
+++ frameworks/huks_standard/main/common/include/hks_message_code.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HKS_MESSAGE_CODE_H
+#define HKS_MESSAGE_CODE_H
+
+enum HksMessage {
+    HKS_MSG_BASE = 0x3a400,
+
+    HKS_MSG_GEN_KEY = HKS_MSG_BASE,
+    HKS_MSG_IMPORT_KEY,
+    HKS_MSG_EXPORT_PUBLIC_KEY,
+    HKS_MSG_IMPORT_WRAPPED_KEY,
+    HKS_MSG_DELETE_KEY,
+    HKS_MSG_GET_KEY_PARAMSET,
+    HKS_MSG_KEY_EXIST,
+    HKS_MSG_GENERATE_RANDOM,
+    HKS_MSG_SIGN,
+    HKS_MSG_VERIFY,
+    HKS_MSG_ENCRYPT,
+    HKS_MSG_DECRYPT,
+    HKS_MSG_AGREE_KEY,
+    HKS_MSG_DERIVE_KEY,
+    HKS_MSG_MAC,
+    HKS_MSG_GET_KEY_INFO_LIST,
+    HKS_MSG_ATTEST_KEY,
+    HKS_MSG_GET_CERTIFICATE_CHAIN,
+    HKS_MSG_INIT,
+    HKS_MSG_UPDATE,
+    HKS_MSG_FINISH,
+    HKS_MSG_ABORT,
+
+    /* new cmd type must be added before HKS_MSG_MAX */
+    HKS_MSG_MAX,
+};
+
+#endif /* HKS_MESSAGE_CODE_H */
--- services/huks_standard/huks_service/main/os_dependency/sa/hks_sa.h
+++ services/huks_standard/huks_service/main/os_dependency/sa/hks_sa.h
@@ -16,6 +16,8 @@
 #ifndef HKS_SA_H
 #define HKS_SA_H
 
+#include "hks_message_code.h"
+
 #include "iremote_broker.h"
 #include "iremote_stub.h"
 #include "nocopyable.h"
@@ -32,36 +34,6 @@ enum ResponseCode {
     HW_NO_ERROR =  0,
     HW_SYSTEM_ERROR = -1,
     HW_PERMISSION_DENIED = -2,
-};
-
-enum HksMessage {
-    HKS_MSG_BASE = 0x3a400,
-
-    HKS_MSG_GEN_KEY = HKS_MSG_BASE,
-    HKS_MSG_IMPORT_KEY,
-    HKS_MSG_EXPORT_PUBLIC_KEY,
-    HKS_MSG_IMPORT_WRAPPED_KEY,
-    HKS_MSG_DELETE_KEY,
-    HKS_MSG_GET_KEY_PARAMSET,
-    HKS_MSG_KEY_EXIST,
-    HKS_MSG_GENERATE_RANDOM,
-    HKS_MSG_SIGN,
-    HKS_MSG_VERIFY,
-    HKS_MSG_ENCRYPT,
-    HKS_MSG_DECRYPT,
-    HKS_MSG_AGREE_KEY,
-    HKS_MSG_DERIVE_KEY,
-    HKS_MSG_MAC,
-    HKS_MSG_GET_KEY_INFO_LIST,
-    HKS_MSG_ATTEST_KEY,
-    HKS_MSG_GET_CERTIFICATE_CHAIN,
-    HKS_MSG_INIT,
-    HKS_MSG_UPDATE,
-    HKS_MSG_FINISH,
-    HKS_MSG_ABORT,
-
-    /* new cmd type must be added before HKS_MSG_MAX */
-    HKS_MSG_MAX,
 };
 
 constexpr int SA_ID_KEYSTORE_SERVICE = 3510;
