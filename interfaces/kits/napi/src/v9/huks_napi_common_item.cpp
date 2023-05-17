610d104ba8a1bb1d87e7f6be7b28ca57f24e8175
CheungVane <zhangwenzhi3@huawei.com>
2023-05-17 15:10:56 +0800

    为可选参数+callback参数做undefined和null默认处理 Signed-off-by: zhangwenzhi <zhangwenzhi3@huawei.com>

--- interfaces/kits/napi/src/v9/huks_napi_common_item.cpp
+++ interfaces/kits/napi/src/v9/huks_napi_common_item.cpp
@@ -344,9 +344,13 @@ napi_ref GetCallback(napi_env env, napi_value object)
     }
 
     if (valueType != napi_valuetype::napi_function) {
-        napi_throw_error(env, std::to_string(HUKS_ERR_CODE_ILLEGAL_ARGUMENT).c_str(),
-            "the type of callback isn't function");
-        HKS_LOG_E("invalid type");
+        if (valueType != napi_valuetype::napi_null && valueType != napi_valuetype::napi_undefined) {
+            napi_throw_error(env, std::to_string(HUKS_ERR_CODE_ILLEGAL_ARGUMENT).c_str(),
+            "invalid arguments");
+            HKS_LOG_E("the type of callback isn't function");
+        } else {
+            HKS_LOG_I("no callback fun, process as promise func");
+        }
         return nullptr;
     }
 
