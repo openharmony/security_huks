95c22d781d7554f943750224cdd8ef91effd49dd
CheungVane <zhangwenzhi3@huawei.com>
2023-07-04 11:47:48 +0800

    clean data of unused key buffer Signed-off-by: zhangwenzhi <zhangwenzhi3@huawei.com>

--- services/huks_standard/huks_service/main/core/src/hks_client_service.c
+++ services/huks_standard/huks_service/main/core/src/hks_client_service.c
@@ -785,10 +785,11 @@ int32_t HksServiceGenerateKey(const struct HksProcessInfo *processInfo, const st
         ret = HksStoreKeyBlob(processInfo, keyAlias, HKS_STORAGE_TYPE_KEY, &output);
         HKS_IF_NOT_SUCC_LOGE(ret, "store keyblob to storage failed, ret = %" LOG_PUBLIC "d", ret)
     } while (0);
+
+    HKS_FREE_PTR(keyOutBuffer);
     if (keyIn.data != NULL) {
         (void)memset_s(keyIn.data, keyIn.size, 0, keyIn.size);
     }
-    HKS_FREE_PTR(keyOutBuffer);
     HKS_FREE_PTR(keyIn.data);
     HksFreeParamSet(&newParamSet);
     HksHitraceEnd(&traceId);
