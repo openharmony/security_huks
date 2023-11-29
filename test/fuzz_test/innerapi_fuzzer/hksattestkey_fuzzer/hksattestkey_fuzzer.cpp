c3e1292ffbf9622d08b429fd75ac16e2490087db
y30053096 <yangjinhuan@huawei.com>
2023-11-29 19:13:34 +0800

    fix attest and anonymous attest fuzz test
    Signed-off-by: y30053096 <yangjinhuan@huawei.com>

--- test/fuzz_test/innerapi_fuzzer/hksattestkey_fuzzer/hksattestkey_fuzzer.cpp
+++ test/fuzz_test/innerapi_fuzzer/hksattestkey_fuzzer/hksattestkey_fuzzer.cpp
@@ -15,48 +15,40 @@
 #include "hksattestkey_fuzzer.h"
 
 #include <securec.h>
+#include <vector>
 
 #include "hks_api.h"
 #include "hks_mem.h"
 #include "hks_param.h"
 #include "hks_type.h"
 
-const int BLOB_SIZE = 10;
-const int CERT_COUNT = 4;
+constexpr int ALIAS_SIZE = 10;
+constexpr int CERT_SIZE = 4096;
+constexpr int CERT_COUNT = 4;
 
 namespace OHOS {
     bool DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
     {
         if (data == nullptr ||
-            size <= (BLOB_SIZE + sizeof(struct HksParamSet) + sizeof(struct HksBlob) * CERT_COUNT)) {
+            size <= (ALIAS_SIZE + CERT_SIZE * CERT_COUNT) + sizeof(struct HksParamSet)) {
             return false;
         }
-
-        uint8_t *myData = static_cast<uint8_t *>(HksMalloc(sizeof(uint8_t) * size));
-        if (myData == nullptr) {
-            return false;
-        }
-
-        (void)memcpy_s(myData, size, data, size);
-
-        struct HksBlob keyAlias = { BLOB_SIZE, myData };
-
-        struct HksCertChain *certChain =
-            reinterpret_cast<struct HksCertChain *>(HksMalloc(sizeof(struct HksCertChain)));
-        certChain->certsCount = CERT_COUNT;
-        certChain->certs = reinterpret_cast<struct HksBlob *>(myData + BLOB_SIZE);
-
+        std::vector<uint8_t> newData(data, data + size);
+        struct HksBlob keyAlias = { ALIAS_SIZE, newData.data() };
+        struct HksBlob resultCerts[CERT_COUNT] = {
+            { CERT_SIZE, newData.data() + ALIAS_SIZE },
+            { CERT_SIZE, newData.data() + ALIAS_SIZE + CERT_SIZE },
+            { CERT_SIZE, newData.data() + ALIAS_SIZE + CERT_SIZE + CERT_SIZE },
+            { CERT_SIZE, newData.data() + ALIAS_SIZE + CERT_SIZE + CERT_SIZE + CERT_SIZE}
+        };
+        struct HksCertChain certChain = {
+            .certs = resultCerts,
+            .certsCount = CERT_COUNT
+        };
         struct HksParamSet *paramSet =
-            reinterpret_cast<struct HksParamSet *>(myData + BLOB_SIZE + sizeof(struct HksBlob) * CERT_COUNT);
-        paramSet->paramSetSize = size - BLOB_SIZE - sizeof(struct HksBlob) * CERT_COUNT;
-
-        (void)HksAttestKey(&keyAlias, paramSet, certChain);
-
-        if (certChain != nullptr) {
-            HksFree(certChain);
-        }
-
-        HksFree(myData);
+            reinterpret_cast<struct HksParamSet *>(newData.data() + ALIAS_SIZE + CERT_SIZE * CERT_COUNT);
+        paramSet->paramSetSize = size - ALIAS_SIZE - CERT_SIZE * CERT_COUNT;
+        (void)HksAttestKey(&keyAlias, paramSet, &certChain);
         return true;
     }
 }
