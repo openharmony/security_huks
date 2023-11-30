b3f8bd93e87f892a1d3d69ad12f39998435497ac
wuXin <wuxintao5@huawei.com>
2023-11-30 11:48:26 +0000

    update utils/file_operator/hks_file_operator.c. add code for  fix write File bug Signed-off-by: wuxintao <wuxintao5@huawei.com>
    Signed-off-by: wuXin <wuxintao5@huawei.com>

--- utils/file_operator/hks_file_operator.c
+++ utils/file_operator/hks_file_operator.c
@@ -139,11 +139,8 @@ static int32_t FileWrite(const char *fileName, uint32_t offset, const uint8_t *b
         return HKS_ERROR_INVALID_KEY_FILE;
     }
 
-    if (realpath(fileName, filePath) == NULL) {
-        HKS_LOG_E("invalid filePath, path!");
-        return HKS_ERROR_INVALID_KEY_FILE;
-    }
-
+    (void)realpath(fileName, filePath);
+ 
     /* caller function ensures that the folder exists */
     FILE *fp = fopen(filePath, "wb+");
     HKS_IF_NULL_LOGE_RETURN(fp, HKS_ERROR_OPEN_FILE_FAIL, "open file fail, errno = 0x%" LOG_PUBLIC "x", errno)
