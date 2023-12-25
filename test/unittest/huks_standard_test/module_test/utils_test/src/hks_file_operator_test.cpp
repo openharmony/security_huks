622290c4d15cd917246a4606eddbcfc595588c9a
tangboyi <tangboyi1@huawei.com>
2023-12-25 10:18:05 +0800

    新增size指针判空
    Signed-off-by: tangboyi <tangboyi1@huawei.com>
    Change-Id: I8d58ceaecf2641d5c2cddf143abe3d30aa4a352d

--- test/unittest/huks_standard_test/module_test/utils_test/src/hks_file_operator_test.cpp
+++ test/unittest/huks_standard_test/module_test/utils_test/src/hks_file_operator_test.cpp
@@ -88,7 +88,7 @@ HWTEST_F(HksFileOperatorTest, HksFileOperatorTest002, TestSize.Level0)
 HWTEST_F(HksFileOperatorTest, HksFileOperatorTest003, TestSize.Level0)
 {
     HKS_LOG_I("enter HksFileOperatorTest003");
-    int32_t ret = HksFileRead(nullptr, nullptr, 0, nullptr, 0);
+    int32_t ret = HksFileRead(nullptr, nullptr, 0, nullptr, nullptr);
     ASSERT_EQ(ret, HKS_ERROR_INVALID_ARGUMENT) << "HksFileRead failed, ret = " << ret;
 }
 
