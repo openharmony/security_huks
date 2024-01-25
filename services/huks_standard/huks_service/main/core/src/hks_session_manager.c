f0d0ca033c38dcd3de0648c439c34497aae11e3a
tangboyi <tangboyi1@huawei.com>
2024-01-25 19:24:42 +0800

    修改日志打印规格，新增session id取代时间戳
    Signed-off-by: tangboyi <tangboyi1@huawei.com>
    Change-Id: I937e94a87a0361af5f9708305b45398a643ea888

--- services/huks_standard/huks_service/main/core/src/hks_session_manager.c
+++ services/huks_standard/huks_service/main/core/src/hks_session_manager.c
@@ -137,7 +137,7 @@ static int32_t AddOperation(struct HksOperation *operation)
 
     AddNodeAtDoubleListTail(&g_operationList, &operation->listHead);
     ++g_operationCount;
-    HKS_LOG_I("add operation count:%" LOG_PUBLIC "u", g_operationCount);
+    HKS_LOG_D("add operation count:%" LOG_PUBLIC "u", g_operationCount);
     pthread_mutex_unlock(&g_lock);
     return HKS_SUCCESS;
 }
@@ -334,7 +334,7 @@ void DeleteOperation(const struct HksBlob *operationHandle)
             }
             FreeOperation(&operation);
             --g_operationCount;
-            HKS_LOG_I("delete operation count:%" LOG_PUBLIC "u", g_operationCount);
+            HKS_LOG_D("delete operation count:%" LOG_PUBLIC "u", g_operationCount);
             pthread_mutex_unlock(&g_lock);
             return;
         }
