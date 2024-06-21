4f4ce1d8f4e532e186e4e21580c3165704200082
WangShengming <wangshengming6@huawei.com>
2024-06-21 17:45:39 +0800

    add code
    Signed-off-by: WangShengming <wangshengming6@huawei.com>

--- interfaces/kits/c/include/native_huks_type.h
+++ interfaces/kits/c/include/native_huks_type.h
@@ -656,7 +656,7 @@ enum OH_Huks_Tag {
     OH_HUKS_TAG_AUTH_STORAGE_LEVEL = OH_HUKS_TAG_TYPE_UINT | 316,
     
     /**
-     * Authentication mode of the user authtoken，whose optional values are from enum HuksUserAuthMode.
+     * Authentication mode of the user authtoken, whose optional values are from enum HuksUserAuthMode.
      *
      * @since 12
      */
