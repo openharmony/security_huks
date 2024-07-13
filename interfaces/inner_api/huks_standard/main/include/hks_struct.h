f1ea84d064d035e6e1492cf20ba75acee332788b
wpy88 <2423811583@qq.com>
2024-07-13 18:13:27 +0800

    split hks_type.h
    Signed-off-by: wpy88 <2423811583@qq.com>

---
+++ interfaces/inner_api/huks_standard/main/include/hks_struct.h
@@ -0,0 +1,365 @@
+/*
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file hks_struct.h
+ *
+ * @brief Declares huks struct and enum.
+ *
+ * @since 8
+ */
+
+#ifndef HKS_STRUCT_H
+#define HKS_STRUCT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Align to 4-tuple
+ * Before calling this function, ensure that the size does not overflow after 3 is added.
+ */
+#define ALIGN_SIZE(size) ((((uint32_t)(size) + 3) >> 2) << 2)
+#define DEFAULT_ALIGN_MASK_SIZE 3
+
+#define HKS_AE_TAG_LEN 16
+#define HKS_BITS_PER_BYTE 8
+#define MAX_KEY_SIZE 2048
+#define HKS_AE_TAG_LEN 16
+#define HKS_AE_NONCE_LEN 12
+#define HKS_MAX_KEY_ALIAS_LEN 64
+#define HKS_MAX_PROCESS_NAME_LEN 50
+#define HKS_MAX_RANDOM_LEN 1024
+#define HKS_KEY_BYTES(keySize) (((keySize) + HKS_BITS_PER_BYTE - 1) / HKS_BITS_PER_BYTE)
+#define HKS_SIGNATURE_MIN_SIZE 64
+#define HKS_ARRAY_SIZE(arr) ((sizeof(arr)) / (sizeof((arr)[0])))
+
+#define HKS_WRAPPED_FORMAT_MAX_SIZE (1024 * 1024)
+#define HKS_IMPORT_WRAPPED_KEY_TOTAL_BLOBS 10
+
+#define TOKEN_CHALLENGE_LEN 32
+#define UDID_LEN 64
+#define SHA256_SIGN_LEN 32
+#define TOKEN_SIZE 32
+#define MAX_AUTH_TIMEOUT_SECOND 60
+#define SECURE_SIGN_VERSION 0x01000001
+
+#define HKS_CERT_COUNT 4
+#define HKS_CERT_ROOT_SIZE 2048
+#define HKS_CERT_CA_SIZE 2048
+#define HKS_CERT_DEVICE_SIZE 2048
+#define HKS_CERT_APP_SIZE 4096
+
+#define HKS_MAX_FILE_SIZE 10240
+
+#define HKS_KEY_BLOB_AT_KEY_SIZE 256
+#define HKS_KEY_BLOB_AT_KEY_BYTES 32
+
+#define HKS_MAX_KEY_ALIAS_COUNT 2048
+
+/**
+ * @brief hks key algorithm
+ */
+enum HksKeyAlg {
+    HKS_ALG_RSA = 1,
+    HKS_ALG_ECC = 2,
+    HKS_ALG_DSA = 3,
+
+    HKS_ALG_AES = 20,
+    HKS_ALG_HMAC = 50,
+    HKS_ALG_HKDF = 51,
+    HKS_ALG_PBKDF2 = 52,
+    HKS_ALG_GMKDF = 53,
+
+    HKS_ALG_ECDH = 100,
+    HKS_ALG_X25519 = 101,
+    HKS_ALG_ED25519 = 102,
+    HKS_ALG_DH = 103,
+
+    HKS_ALG_SM2 = 150,
+    HKS_ALG_SM3 = 151,
+    HKS_ALG_SM4 = 152,
+};
+
+/**
+ * @brief hks user auth type
+ * @see `enum AuthType` in `drivers/interface/user_auth/v2_0/UserAuthTypes.idl`
+ */
+enum HksUserAuthType {
+    HKS_USER_AUTH_TYPE_FINGERPRINT = 1 << 0,
+    HKS_USER_AUTH_TYPE_FACE = 1 << 1,
+    HKS_USER_AUTH_TYPE_PIN = 1 << 2,
+};
+
+/**
+ * @brief hks blob
+ */
+struct HksBlob {
+    uint32_t size;
+    uint8_t *data;
+};
+
+/**
+ * @brief hks param
+ */
+struct HksParam {
+    uint32_t tag;
+    union {
+        bool boolParam;
+        int32_t int32Param;
+        uint32_t uint32Param;
+        uint64_t uint64Param;
+        struct HksBlob blob;
+    };
+};
+
+/**
+ * @brief hks param set
+ */
+struct HksParamSet {
+    uint32_t paramSetSize;
+    uint32_t paramsCnt;
+    struct HksParam params[];
+};
+
+/**
+ * @brief hks certificate chain
+ */
+struct HksCertChain {
+    struct HksBlob *certs;
+    uint32_t certsCount;
+};
+
+/**
+ * @brief hks key info
+ */
+struct HksKeyInfo {
+    struct HksBlob alias;
+    struct HksParamSet *paramSet;
+};
+
+/**
+ * @brief hks public key info
+ */
+struct HksPubKeyInfo {
+    enum HksKeyAlg keyAlg;
+    uint32_t keySize;
+    uint32_t nOrXSize;
+    uint32_t eOrYSize;
+    uint32_t placeHolder;
+};
+
+/**
+ * @brief hks rsa key material
+ */
+struct HksKeyMaterialRsa {
+    enum HksKeyAlg keyAlg;
+    uint32_t keySize;
+    uint32_t nSize;
+    uint32_t eSize;
+    uint32_t dSize;
+};
+
+/**
+ * @brief hks ecc key material
+ */
+struct HksKeyMaterialEcc {
+    enum HksKeyAlg keyAlg;
+    uint32_t keySize;
+    uint32_t xSize;
+    uint32_t ySize;
+    uint32_t zSize;
+};
+
+/**
+ * @brief hks dsa key material
+ */
+struct HksKeyMaterialDsa {
+    enum HksKeyAlg keyAlg;
+    uint32_t keySize;
+    uint32_t xSize;
+    uint32_t ySize;
+    uint32_t pSize;
+    uint32_t qSize;
+    uint32_t gSize;
+};
+
+/**
+ * @brief hks dh key material
+ */
+struct HksKeyMaterialDh {
+    enum HksKeyAlg keyAlg;
+    uint32_t keySize;
+    uint32_t pubKeySize;
+    uint32_t priKeySize;
+    uint32_t reserved;
+};
+
+/**
+ * @brief hks 25519 key material
+ */
+struct HksKeyMaterial25519 {
+    enum HksKeyAlg keyAlg;
+    uint32_t keySize;
+    uint32_t pubKeySize;
+    uint32_t priKeySize;
+    uint32_t reserved;
+};
+
+/**
+ * @brief hks user auth token plaintext data
+ * @see `TokenDataPlain` in `drivers/peripheral/user_auth/hdi_service/user_auth/inc/user_sign_centre.h`
+ */
+typedef struct HksPlaintextData {
+    uint8_t challenge[TOKEN_SIZE];
+    uint64_t time;
+    uint32_t authTrustLevel;
+    uint32_t authType;
+    uint32_t authMode;
+    uint32_t securityLevel;
+    /**
+     * @see `enum TokenType` in `drivers/peripheral/user_auth/hdi_service/common/inc/defines.h`
+     */
+    uint32_t tokenType;
+} __attribute__((__packed__)) HksPlaintextData;
+
+/**
+ * @brief hks user auth token ciphertext data
+ * @see `TokenDataToEncrypt` in `drivers/peripheral/user_auth/hdi_service/user_auth/inc/user_sign_centre.h`
+ */
+typedef struct HksCiphertextData {
+    int32_t userId;
+    uint64_t secureUid;
+    uint64_t enrolledId;
+    uint64_t credentialId;
+    uint8_t collectorUdid[UDID_LEN];
+    uint8_t verifierUdid[UDID_LEN];
+} __attribute__((__packed__)) HksCiphertextData;
+
+/**
+ * @brief hks user auth token
+ * @see `UserAuthTokenHal` in `drivers/peripheral/user_auth/hdi_service/user_auth/inc/user_sign_centre.h`
+ */
+typedef struct __attribute__((__packed__)) HksUserAuthToken {
+    uint32_t version;
+    HksPlaintextData plaintextData;
+    HksCiphertextData ciphertextData;
+    uint8_t tag[HKS_AE_TAG_LEN];
+    uint8_t iv[HKS_AE_NONCE_LEN];
+    uint8_t sign[SHA256_SIGN_LEN];
+} __attribute__((__packed__)) HksUserAuthToken;
+
+/**
+ * @brief hks user auth token key
+ */
+struct HksAuthTokenKey {
+    uint8_t macKey[HKS_KEY_BLOB_AT_KEY_BYTES];
+    uint8_t cipherKey[HKS_KEY_BLOB_AT_KEY_BYTES];
+};
+
+/**
+ * @brief hks secure sign auth info
+ */
+typedef struct __attribute__((__packed__)) HksSecureSignAuthInfo {
+    uint32_t userAuthType;
+    uint64_t authenticatorId;
+    uint64_t credentialId;
+} __attribute__((__packed__)) HksSecureSignAuthInfo;
+
+enum HksUserIamType {
+    HKS_AUTH_TYPE = 0,
+};
+
+struct EnrolledInfoWrap {
+    enum HksUserAuthType authType;
+    uint64_t enrolledId;
+};
+
+struct SecInfoWrap {
+    uint64_t secureUid;
+    uint32_t enrolledInfoLen;
+    struct EnrolledInfoWrap *enrolledInfo;
+};
+
+/**
+ * @brief hks alias set
+ */
+struct HksKeyAliasSet {
+    uint32_t aliasesCnt;
+    struct HksBlob *aliases;
+};
+
+
+#define HKS_DERIVE_DEFAULT_SALT_LEN 16
+#define HKS_HMAC_DIGEST_SHA512_LEN 64
+#define HKS_DEFAULT_RANDOM_LEN 16
+#define HKS_MAX_KEY_AUTH_ID_LEN 64
+#define HKS_KEY_MATERIAL_NUM 3
+#define HKS_MAX_KEY_LEN (HKS_KEY_BYTES(HKS_RSA_KEY_SIZE_4096) * HKS_KEY_MATERIAL_NUM)
+#define HKS_MAX_KEY_MATERIAL_LEN (sizeof(struct HksPubKeyInfo) + HKS_MAX_KEY_LEN + HKS_AE_TAG_LEN)
+
+/**
+ * @brief hks store header info
+ */
+struct HksStoreHeaderInfo {
+    uint16_t version;
+    uint16_t keyCount;
+    uint32_t totalLen; /* key buffer total len */
+    uint32_t sealingAlg;
+    uint8_t salt[HKS_DERIVE_DEFAULT_SALT_LEN];
+    uint8_t hmac[HKS_HMAC_DIGEST_SHA512_LEN];
+};
+
+/**
+ * @brief hks store key info
+ */
+struct HksStoreKeyInfo {
+    uint16_t keyInfoLen; /* current keyinfo len */
+    uint16_t keySize;    /* keySize of key from crypto hal after encrypted */
+    uint8_t random[HKS_DEFAULT_RANDOM_LEN];
+    uint8_t flag;        /* import or generate key */
+    uint8_t keyAlg;
+    uint8_t keyMode;
+    uint8_t digest;
+    uint8_t padding;
+    uint8_t rsv;
+    uint16_t keyLen;     /* keyLen from paramset, e.g. aes-256 */
+    uint32_t purpose;
+    uint32_t role;
+    uint16_t domain;
+    uint8_t aliasSize;
+    uint8_t authIdSize;
+};
+
+/**
+ * @brief hks 25519 key pair
+ */
+struct Hks25519KeyPair {
+    uint32_t publicBufferSize;
+    uint32_t privateBufferSize;
+};
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HKS_STRUCT_H */
