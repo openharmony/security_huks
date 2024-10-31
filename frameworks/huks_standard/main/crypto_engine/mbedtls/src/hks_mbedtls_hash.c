2f35ab9f5a3d23416b6c737402e3434388282209
tangboyi <tangboyi1@huawei.com>
2024-10-31 21:00:50 +0800

    huks mbedtls适配Hi3861
    Signed-off-by: tangboyi <tangboyi1@huawei.com>
    Change-Id: I013571822d6b093223bb3e90825a176b0064d1b2

--- frameworks/huks_standard/main/crypto_engine/mbedtls/src/hks_mbedtls_hash.c
+++ frameworks/huks_standard/main/crypto_engine/mbedtls/src/hks_mbedtls_hash.c
@@ -12,8 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#ifndef USE_HISI_MBED
 #include "mbedtls/compat-2.x.h"
+#endif
 #ifdef HKS_CONFIG_FILE
 #include HKS_CONFIG_FILE
 #else
