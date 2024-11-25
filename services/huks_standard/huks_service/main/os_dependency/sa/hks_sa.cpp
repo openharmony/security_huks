17f4161508f0cf56701c3368b7d9b41418b5c66f
wpy88 <weipengyu4@huawei.com>
2024-11-25 20:41:25 +0800

    add ipc num
    Signed-off-by: wpy88 <weipengyu4@huawei.com>
    Change-Id: I3b571a59feee8b74904b47907c21e21d6503000b

--- services/huks_standard/huks_service/main/os_dependency/sa/hks_sa.cpp
+++ services/huks_standard/huks_service/main/os_dependency/sa/hks_sa.cpp
@@ -68,7 +68,7 @@ REGISTER_SYSTEM_ABILITY_BY_ID(HksService, SA_ID_KEYSTORE_SERVICE, true);
 std::mutex HksService::instanceLock;
 sptr<HksService> HksService::instance;
 const uint32_t MAX_MALLOC_LEN = 1 * 1024 * 1024; /* max malloc size 1 MB */
-#define HUKS_IPC_THREAD_NUM 2
+#define HUKS_IPC_THREAD_NUM 16
 #ifdef SUPPORT_COMMON_EVENT
 const uint32_t MAX_DELAY_TIMES = 100;
 #endif
