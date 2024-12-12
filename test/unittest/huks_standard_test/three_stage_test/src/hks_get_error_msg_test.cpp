1d0d3b02875517c12bf1961c3a626e360944bfb2
wpy88 <weipengyu4@huawei.com>
2024-12-12 16:50:28 +0800

    getErrorMsg TDD Fuzz
    Signed-off-by: wpy88 <weipengyu4@huawei.com>
    Change-Id: I35158891092482b18e3b4845e5abd2e157814b83

---
+++ test/unittest/huks_standard_test/three_stage_test/src/hks_get_error_msg_test.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "hks_api.h"
+#include "hks_param.h"
+#include "hks_test_common.h"
+#include "hks_test_log.h"
+#include "hks_type.h"
+#include "hks_log.h"
+#include "hks_mem.h"
+#include "nativetoken_kit.h"
+#include "token_setproc.h"
+using namespace testing::ext;
+namespace {
+class HksGetErrorMsgTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+
+    static void TearDownTestCase(void);
+
+    void SetUp();
+
+    void TearDown();
+};
+
+void HksGetErrorMsgTest::SetUpTestCase(void)
+{
+}
+
+void HksGetErrorMsgTest::TearDownTestCase(void)
+{
+}
+
+void HksGetErrorMsgTest::SetUp()
+{
+}
+
+void HksGetErrorMsgTest::TearDown()
+{
+}
+
+/**
+ * @tc.name: HksGetErrorMsgTest.HksGetErrorMsgTest001
+ * @tc.desc: call HksGetErrorMsg;
+ * @tc.type: FUNC
+ */
+HWTEST_F(HksGetErrorMsgTest, HksGetErrorMsgTest001, TestSize.Level0)
+{
+    const char *errorMsg = HksGetErrorMsg();
+    EXPECT_NE(errorMsg, nullptr) << "HksGetErrorMsg ret is nullptr";
+}
+
+}
