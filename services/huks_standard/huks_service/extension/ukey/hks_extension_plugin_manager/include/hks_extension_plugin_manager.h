74843845125872f75a8c13ca6df16053e39d65e0
zhuke <zhuke27@huawei.com>
2025-09-04 15:40:18 +0800

    change extension manager interfaces
    Signed-off-by: zhuke <zhuke27@huawei.com>
    Change-Id: Idf9189cf2c3b0441277b7ec255a97d358ec53dd6

--- services/huks_standard/huks_service/extension/ukey/hks_extension_plugin_manager/include/hks_extension_plugin_manager.h
+++ services/huks_standard/huks_service/extension/ukey/hks_extension_plugin_manager/include/hks_extension_plugin_manager.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2025-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HKS_PROVIDER_LIFE_CYCLE_MANAGER_H
+#define HKS_PROVIDER_LIFE_CYCLE_MANAGER_H
+
 #include <string>
 #include <unordered_map>
 
@@ -13,24 +31,27 @@ std::string RegisterFunName = "_ZN27HksProviderLifeCycleManager18OnRegisterProvi
 
 
 
-using RegisterFun = int32_t (*)(const HksProcessInfo &processInfo, const std::string &providerName,
-    const CppParamSet &paramSet);
-
-class HuksExtensionPluginManager{
-private:
-    void* m_pluginHandle = nullptr; // 存储 dlopen 返回的句柄
-    static std::once_flag initFlag;
-    std::unordered_map<std::string, RegisterFun>m_pluginProviderMap;
-    //OHOS::SafeMap<std::string, std::shared_ptr<CrypoExtensionProxy>> m_pluginProviderMap;
-
-    //CreateInstance()
-   // GetInstance()
-    int32_t LoadPlugins(const std::string& AbilityName);
-    int32_t RecordPlugin();
-    int32_t RemovePlugin();
-    int32_t ExecutePlugin();
+namespace OHOS {
+namespace Security {
+namespace Huks {
+    class HuksExtensionPluginManager: private OHOS::DelayedSingleton<HuksExtensionPluginManager>{
+public:
+    std::unordered_map<PluginMethodEnum, void*> m_pluginProviderMap;
+    static std::shared_ptr<HksProviderLifeCycleManager> GetInstanceWrapper();
+    int32_t RegisterProvider(struct HksProcessInfo &info, const std::string &AbilityName,
+    const CppParamSet& paramSet);
+    int32_t UnRegisterProvider(struct HksProcessInfo &info, const std::string &AbilityName,
+    const CppParamSet& paramSet);
+    int32_t OnSigned(const std::string &index, const CppParamSet& paramSet, vector<uint8_t> &outData);
+    int32_t OnVerifyPin(const std::string &index, vector<uint8_t> &pinData);
+    int32_t OnFindCertificate(const std::string &index, vector<uint8_t> &cetificatesOut);
+    int32_t OnEncryptData(const std::string &index, const CppParamSet& paramSet, vector<uint8_t> &outData);
+    int32_t OnDecryptData(const std::string &index, const CppParamSet& paramSet, vector<uint8_t> &outData);
 
 public:
     int32_t RegisterProvider(struct HksProcessInfo &info, const std::string &AbilityName,
     const CppParamSet& paramSet);
 };
+}
+}
+}
--- services/huks_standard/huks_service/extension/ukey/hks_extension_plugin_manager/src/hks_extension_plugin_manager.cpp
+++ services/huks_standard/huks_service/extension/ukey/hks_extension_plugin_manager/src/hks_extension_plugin_manager.cpp
@@ -1,6 +1,30 @@
+/*
+ * Copyright (c) 2025-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "hks_extension_plugin_manager.h"
 
-std::once_flag HuksExtensionPluginManager::initFlag;
+#include <vector>
+// -999 没找到函数
+//
+
+namespace OHOS::Security::Huks {
+
+std::shared_ptr<HuksExtensionPluginManager> HuksExtensionPluginManager::GetInstanceWrapper()
+{
+    return HuksExtensionPluginManager::GetInstance();
+}
 
 int32_t HuksExtensionPluginManager::RegisterProvider(struct HksProcessInfo &info, const std::string& AbilityName,
     const CppParamSet& paramSet){
@@ -16,14 +40,42 @@ int32_t HuksExtensionPluginManager::RegisterProvider(struct HksProcessInfo &info
         return ret;
     }
 
-    // 后续调用（包括第一次初始化成功后的调用）的实际业务逻辑
-    // 例如：根据 AbilityName 从 map 中查找函数指针并调用
-    auto it = m_pluginProviderMap.find(AbilityName);
-    if (it != m_pluginProviderMap.end()) {
-        RegisterFun funcPtr = reinterpret_cast<RegisterFun>(it->second);
-        if (funcPtr != nullptr) {
-            return funcPtr(info, AbilityName, paramSet); // 调用动态库中的函数
+    auto it = m_pluginProviderMap.find(PluginMethodEnum::FUNC_ON_REGISTERPROVIDE);
+    HKS_IF_TRUE_LOGE_RETURN(it == m_pluginProviderMap.End() || it->second == nullptr,
+        HKS_ERROR_FIND_FUNC_MAP_FAIL, "method OnRegisterProvider is not found")
+
+    int32_t ret = (*reinterpret_cast<OnRegisterProviderFunc>(it->second))(info, AbilityName, paramSet);
+    HKS_IF_TRUE_LOGE_RETURN(ret != HKS_SUCCESS, ret,
+        "OnRegisterProvider fail, lib can not be loaded now, ret = %{public}d", ret)
+    HKS_LOG_I("regist provider success")
+    return HKS_SUCCESS;
+}
+
+int32_t HuksExtensionPluginManager::LoadPlugins(){
+    m_pluginHandle = dlopen(PLUGIN_SO, RTLD_NOW);
+    HKS_IF_NULL_LOGE_RETURN(m_pluginHandle, HKS_ERROR_OPEN_LIB_FAIL, "dlopen " PLUGIN_SO " failed! %" LOG_PUBLIC "s", dlerror())
+
+    for (auto i = 0; i < static_cast<int>(PluginMethodEnum::COUNT); ++i) {
+        std::string methodString = GetMethodByEnum(static_cast<PluginMethodEnum>(i));
+        if (methodString.empty()) {
+            HKS_LOG_E("the entry %{public}d is not include", PLUGIN_SO)
+            dlclose(m_pluginHandle);
+            m_pluginHandle = nullptr;
+            m_pluginProviderMap.clear();
+            return HKS_ERROR_FIND_FUNC_MAP_FAIL;
         }
+        dlerror();
+        void* func = dlsym(m_pluginHandle, methodString.c_str());
+        const char *dlsym_error = dlerror();
+        if (dlsym_error != nullptr) {
+            HKS_LOG_E("failed to find entry %{public}d in dynamic link liberary, error is %{public}d",
+                methodString.c_str(), dlsym_error)
+            dlclose(m_pluginHandle);
+            m_pluginHandle = nullptr;
+            m_pluginProviderMap.clear();
+            return HKS_ERROR_GET_FUNC_POINTER_FAIL;
+        }
+        m_pluginProviderMap.emplace(std::make_pair(static_cast<PluginMethodEnum>(i), func));
     }
     // 没有找到对应的函数指针，返回错误
     return -1;
@@ -31,4 +83,79 @@ int32_t HuksExtensionPluginManager::RegisterProvider(struct HksProcessInfo &info
 
 int32_t HuksExtensionPluginManager::LoadPlugins(const std::string& AbilityName){
 
+    m_pluginProviderMap.Clear();
+    dlclose(m_pluginHandle);
+    m_pluginHandle == nullptr;
+    return HKS_SUCCESS;
+}
+
+static std::string HuksExtensionPluginManager::GetMethodByEnum(PluginMethodEnum methodEnum) {
+    const auto& it = m_pluginMethodNameMap.find(methodEnum);
+    HKS_IF_TRUE_RETURN(it != m_pluginMethodNameMap.end(), it->second)
+    HKS_LOG_E("enum = %{public}d can not find string", methodEnum)
+    return "";
+}
+
+int32_t HuksExtensionPluginManager::OnSigned(const std::string &index,
+    const CppParamSet& paramSet, vector<uint8_t> &outData) {
+    auto it = m_pluginProviderMap.find(PluginMethodEnum::FUNC_ON_SIGNED);
+    HKS_IF_TRUE_LOGE_RETURN(it == m_pluginProviderMap.end(), HKS_ERROR_FIND_FUNC_MAP_FAIL,
+        "Signed method enum not found in plugin provider map.")
+    
+    int ret = (*reinterpret_cast<OnSignedFunc>(it->second))();
+    HKS_IF_TRUE_LOGE_RETURN(ret != HKS_SUCCESS, ret,
+        "Signedfunc fail, lib can not be loaded now, ret = %{public}d", ret)
+    HKS_LOG_I("sign data success")
+
+    return HKS_SUCCESS;
+}
+
+int32_t HuksExtensionPluginManager::OnVerifyPin(const std::string &index, vector<uint8_t> &pinData) {
+    auto it = m_pluginProviderMap.find(PluginMethodEnum::FUNC_ON_VERIFYPIN);
+    HKS_IF_TRUE_LOGE_RETURN(it == m_pluginProviderMap.end(), HKS_ERROR_FIND_FUNC_MAP_FAIL,
+        "VerifyPin method enum not found in plugin provider map.")
+    
+    int ret = (*reinterpret_cast<OnVerifyPinFunc>(it->second))();
+    HKS_IF_TRUE_LOGE_RETURN(ret != HKS_SUCCESS, ret,
+        "VerifyPinfunc fail, lib can not be loaded now, ret = %{public}d", ret)
+    HKS_LOG_I("verify pin success")
+    return HKS_SUCCESS;
+}
+
+int32_t HuksExtensionPluginManager::OnFindCertificate(const std::string &index, vector<uint8_t> &cetificatesOut) {
+    auto it = m_pluginProviderMap.find(PluginMethodEnum::FUNC_ON_FINDCERTIFICATE);
+    HKS_IF_TRUE_LOGE_RETURN(it == m_pluginProviderMap.end(), HKS_ERROR_FIND_FUNC_MAP_FAIL,
+        "FindCertificate method enum not found in plugin provider map.")
+    
+    int ret = (*reinterpret_cast<OnFindCertificate>(it->second))();
+    HKS_IF_TRUE_LOGE_RETURN(ret != HKS_SUCCESS, ret,
+        "FindCertificatefunc fail, lib can not be loaded now, ret = %{public}d", ret)
+    HKS_LOG_I("find certificates success")
+    return HKS_SUCCESS;
+}
+    
+int32_t HuksExtensionPluginManager::OnEncryptData(const std::string &index,
+    const CppParamSet& paramSet, vector<uint8_t> &outData) {
+    auto it = m_pluginProviderMap.find(PluginMethodEnum::FUNC_ON_ENCYPTDATA);
+    HKS_IF_TRUE_LOGE_RETURN(it == m_pluginProviderMap.end(), HKS_ERROR_FIND_FUNC_MAP_FAIL,
+        "EncryptData method enum not found in plugin provider map.")
+    
+    int ret = (*reinterpret_cast<OnEncryptData>(it->second))();
+    HKS_IF_TRUE_LOGE_RETURN(ret != HKS_SUCCESS, ret,
+        "EncryptDatafunc fail, lib can not be loaded now, ret = %{public}d", ret)
+    HKS_LOG_I("encypt data success")
+    return HKS_SUCCESS;
+}
+
+int32_t HuksExtensionPluginManager::OnDecryptData(const std::string &index,
+    const CppParamSet& paramSet, vector<uint8_t> &outData) {
+    auto it = m_pluginProviderMap.find(PluginMethodEnum::FUNC_ON_DECYPTDATA);
+    HKS_IF_TRUE_LOGE_RETURN(it == m_pluginProviderMap.end(), HKS_ERROR_FIND_FUNC_MAP_FAIL,
+        "DecryptData method enum not found in plugin provider map.")
+    
+    int ret = (*reinterpret_cast<OnDecryptData>(it->second))();
+    HKS_IF_TRUE_LOGE_RETURN(ret != HKS_SUCCESS, ret,
+        "DecryptDatafunc fail, lib can not be loaded now, ret = %{public}d", ret)
+    HKS_LOG_I("decypt data success")
+    return HKS_SUCCESS;
 }
