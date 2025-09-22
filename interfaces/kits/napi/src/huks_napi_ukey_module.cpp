d3a73abe3cc06303f8d23ba442e921a1bbb3924b
x00945367 <xuhao237@huawei.com>
2025-09-22 20:58:42 +0800

    move tag for huks_napi to huks_napi_ukey_module
    Signed-off-by: x00945367 <xuhao237@huawei.com>
    Change-Id: Id84f3b3793cb7d90fe157188d513ea02bbb29712

--- interfaces/kits/napi/src/huks_napi_ukey_module.cpp
+++ interfaces/kits/napi/src/huks_napi_ukey_module.cpp
@@ -1,6 +1,32 @@
 #include "ukey/huks_napi_ukey.h"
+#include "hks_tag.h"
 
 using namespace HuksNapiItem;
+
+// Keep style consistent with main huks_napi.cpp
+namespace {
+inline void AddInt32Property(napi_env env, napi_value object, const char *name, int32_t value)
+{
+    napi_value property = nullptr;
+    NAPI_CALL_RETURN_VOID(env, napi_create_int32(env, value, &property));
+    NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, object, name, property));
+}
+
+static napi_value CreateExtensionAbilityTag(napi_env env)
+{
+    napi_value tag = nullptr;
+    NAPI_CALL(env, napi_create_object(env, &tag));
+    /* ExtensionAbility TAG: 300001 - 300100 */
+    AddInt32Property(env, tag, "HUKS_TAG_REMOTE_DEVICE", HKS_TAG_REMOTE_DEVICE);
+    AddInt32Property(env, tag, "HUKS_TAG_REMOTE_APP", HKS_TAG_REMOTE_APP);
+    AddInt32Property(env, tag, "HUKS_TAG_REMOTE_CONTAINER", HKS_TAG_REMOTE_CONTAINER);
+    AddInt32Property(env, tag, "HUKS_TAG_PIN", HKS_TAG_PIN);
+    AddInt32Property(env, tag, "HUKS_TAG_ABILITY_NAME", HKS_TAG_ABILITY_NAME);
+    AddInt32Property(env, tag, "HUKS_TAG_REMOTE_ABILITY_SN", HKS_TAG_REMOTE_ABILITY_SN);
+    AddInt32Property(env, tag, "HUKS_TAG_EXTRA_DATA", HKS_TAG_EXTRA_DATA);
+    return tag;
+}
+} // anonymous namespace
 
 extern "C" {
 static napi_value HuksExternalCryptoRegister(napi_env env, napi_value exports)
@@ -12,6 +38,9 @@ static napi_value HuksExternalCryptoRegister(napi_env env, napi_value exports)
         DECLARE_NAPI_FUNCTION("getUkeyPinAuthState", HuksNapiGetUkeyPinAuthState),
     };
     NAPI_CALL(env, napi_define_properties(env, exports, sizeof(funcDesc) / sizeof(funcDesc[0]), funcDesc));
+
+    napi_value extTag = CreateExtensionAbilityTag(env);
+    NAPI_CALL(env, napi_set_named_property(env, exports, "ExtensionAbilityTag", extTag));
     return exports;
 }
 
