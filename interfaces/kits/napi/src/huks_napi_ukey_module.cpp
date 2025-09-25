f1cc355393e39189b96f1719f9d01a2e2e34708a
linzs <linzhaosheng@huawei.com>
2025-09-25 11:15:26 +0800

    napi bug fixed
    Signed-off-by: linzs <linzhaosheng@huawei.com>
    Change-Id: I607352f28ae1c4a58251b131ed9f994e0de2e298

--- interfaces/kits/napi/src/huks_napi_ukey_module.cpp
+++ interfaces/kits/napi/src/huks_napi_ukey_module.cpp
@@ -1,3 +1,5 @@
+#include "hks_template.h"
+#include "js_native_api.h"
 #include "ukey/huks_napi_ukey.h"
 #include "hks_tag.h"
 
@@ -5,24 +7,28 @@ using namespace HuksNapiItem;
 
 extern "C" {
 
-static napi_value CreateByte(napi_env env, int32_t value)
+#define HKS_NAPI_CALL_FAIL_RETURN(theCall) \
+    if ((theCall) != napi_ok) { \
+        return nullptr; \
+    }
+
+static napi_value CreateU32(napi_env env, uint32_t value)
 {
     napi_value napiValue{};
-    NAPI_CALL(env, napi_create_object(env, &napiValue));
-
+    HKS_NAPI_CALL_FAIL_RETURN(napi_create_uint32(env, value, &napiValue));
     return napiValue;
 }
 
 static napi_value HuksExternalCryptoRegister(napi_env env, napi_value exports)
 {
     napi_property_descriptor propDesc[] = {
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_DEVICE", CreateByte(env, HKS_TAG_REMOTE_DEVICE)),
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_APP", CreateByte(env, HKS_TAG_REMOTE_APP)),
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_CONTAINER", CreateByte(env, HKS_TAG_REMOTE_CONTAINER)),
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_PIN", CreateByte(env, HKS_TAG_PIN)),
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_ABILITY_NAME", CreateByte(env, HKS_TAG_ABILITY_NAME)),
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_ABILITY_SN", CreateByte(env, HKS_TAG_REMOTE_ABILITY_SN)),
-        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_EXTRA_DATA", CreateByte(env, HKS_TAG_EXTRA_DATA)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_DEVICE", CreateU32(env, HKS_TAG_REMOTE_DEVICE)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_APP", CreateU32(env, HKS_TAG_REMOTE_APP)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_CONTAINER", CreateU32(env, HKS_TAG_REMOTE_CONTAINER)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_PIN", CreateU32(env, HKS_TAG_PIN)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_ABILITY_NAME", CreateU32(env, HKS_TAG_ABILITY_NAME)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_REMOTE_ABILITY_SN", CreateU32(env, HKS_TAG_REMOTE_ABILITY_SN)),
+        DECLARE_NAPI_STATIC_PROPERTY("HUKS_TAG_EXTRA_DATA", CreateU32(env, HKS_TAG_EXTRA_DATA)),
 
         DECLARE_NAPI_FUNCTION("registerProvider", HuksNapiRegisterProvider),
         DECLARE_NAPI_FUNCTION("unregisterProvider", HuksNapiUnregisterProvider),
@@ -38,7 +44,7 @@ static napi_module g_externalCryptoModule = {
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = HuksExternalCryptoRegister,
-    .nm_modname = "security.huks.external_crypto",
+    .nm_modname = "security.huksExternalCrypto",
     .nm_priv = nullptr,
     .reserved = { 0 },
 };
