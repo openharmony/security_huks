28754962bd5f5eb624ebcf837634fd2a1b82a597
zw20 <zouwei49@huawei.com>
2025-10-15 10:00:18 +0800

    fix cert interface
    Signed-off-by: zw20 <zouwei49@huawei.com>
    Change-Id: I09b3ba1ec5567d1a593f58435ed53a88ee7b4f9e

---
+++ test/unittest/huks_standard_test/extension_ability_test/ukey_common_test/ukey_common_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+
+#include "hks_log.h"
+#include "hks_json_wrapper.h"
+#include "hks_util.h"
+#include "hks_ukey_common.h"
+
+using namespace testing::ext;
+using namespace OHOS::Security::Huks;
+namespace Unittest::UkeyComm {
+class UkeyCommonTest : pub
+{
+private:
+    /* data */
+public:
+    ukey_common_test(/* args */);
+    ~ukey_common_test();
+};
+    
+    
+}
