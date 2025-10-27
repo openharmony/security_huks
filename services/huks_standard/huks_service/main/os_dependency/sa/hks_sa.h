b127c1af40b72591a4a63e9130a4b5040819ae80
zhuke <zhuke27@huawei.com>
2025-10-27 20:09:11 +0800

    jajja
    Signed-off-by: zhuke <zhuke27@huawei.com>

--- services/huks_standard/huks_service/main/os_dependency/sa/hks_sa.h
+++ services/huks_standard/huks_service/main/os_dependency/sa/hks_sa.h
@@ -77,6 +77,7 @@ public:
     void OnRemoteDied(const wptr<IRemoteObject>& remoteObject) override;
 private:
     void NotifyExtOnBinderDied(int32_t uid);
+    
     int32_t callingPid_;
     int32_t callingUid_;
 };
