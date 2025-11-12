c2aa8c5ee69143ab7a70f91b35c9292320b6c316
wangqiyu1213 <wangqiyu8@huawei.com>
2025-11-12 10:12:04 +0800

    add  HksCryptoExtStubTestAbilityTest
    Signed-off-by: wangqiyu1213 <wangqiyu8@huawei.com>
    Change-Id: I7b8205a7594da40e0e2e893dfab1f4ba326c2d67

--- test/unittest/huks_standard_test/extension_ability_test/extension_native_test/hks_crypto_ext_stub_impl_test.cpp
+++ test/unittest/huks_standard_test/extension_ability_test/extension_native_test/hks_crypto_ext_stub_impl_test.cpp
@@ -50,6 +50,195 @@ HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0000, testing
     CppParamSet params;
     std::string handle;
     int32_t errcode;
-    hksCryptoExtStubImpl.OpenRemoteHandle(index, params, handle, errcode);
+    EXPECT_EQ(hksCryptoExtStubImpl.OpenRemoteHandle(index, params, handle, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.OpenRemoteHandle(index, params, handle, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0001, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    std::string handle;
+    CppParamSet params;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.CloseRemoteHandle(handle, params, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.CloseRemoteHandle(handle, params, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0002, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    std::string index;
+    CppParamSet params;
+    std::string handle;
+    int32_t errcode;
+    int32_t authState;
+    uint32_t retryCnt;
+    EXPECT_EQ(hksCryptoExtStubImpl.AuthUkeyPin(
+        handle, params, errcode, authState, retryCnt), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.AuthUkeyPin(
+        handle, params, errcode, authState, retryCnt), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0003, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    int32_t state;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.GetUkeyPinAuthState(handle, params, state, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.GetUkeyPinAuthState(handle, params, state, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0004, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    int32_t errcode;
+    const std::vector<uint8_t> inData;
+    std::vector<uint8_t> outData;
+    EXPECT_EQ(hksCryptoExtStubImpl.Sign(handle, params, inData, outData, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.Sign(handle, params, inData, outData, errcode), ERR_OK);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0005, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    int32_t errcode;
+    std::vector<uint8_t> plainText;
+    std::vector<uint8_t> signature;
+    EXPECT_EQ(hksCryptoExtStubImpl.Verify(
+        handle, params, plainText, signature, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.Verify(
+        handle, params, plainText, signature, errcode), ERR_OK);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0006, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    std::string index;
+    CppParamSet params;
+    std::string certJsonArr;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.ExportCertificate(
+        index, params, certJsonArr, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.ExportCertificate(index, params, certJsonArr, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0007, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string certJsonArr;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.ExportProviderCertificates(
+        params, certJsonArr, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.ExportProviderCertificates(
+        params, certJsonArr, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0008, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    std::string index;
+    CppParamSet params;
+    std::string handle;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.InitSession(index, params, handle, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.InitSession(index, params, handle, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0009, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    int32_t errcode;
+    std::vector<uint8_t> inData;
+    std::vector<uint8_t> outData;
+    EXPECT_EQ(hksCryptoExtStubImpl.UpdateSession(
+        handle, params, inData, outData, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.UpdateSession(
+        handle, params, inData, outData, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0010, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    std::vector<uint8_t> inData;
+    std::vector<uint8_t> outData;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.FinishSession(
+        handle, params, inData, outData, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.FinishSession(
+        handle, params, inData, outData, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0011, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    std::string propertyId;
+    CppParamSet outParams;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.GetProperty(
+        handle, propertyId, params, outParams, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.GetProperty(
+        handle, propertyId, params, outParams, errcode), HKS_ERROR_EXT_NULLPTR);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0012, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string resourceId;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.GetResourceId(
+        params, resourceId, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.GetResourceId(
+        params, resourceId, errcode), ERR_OK);
+}
+
+HWTEST_F(CryptoExtAbilityStubTest, HksCryptoExtStubTestAbilityTest_0013, testing::ext::TestSize.Level0)
+{
+    std::shared_ptr<HksCryptoExtAbility> extension = std::make_shared<HksCryptoExtAbility>();
+    HksCryptoExtStubImpl hksCryptoExtStubImpl(extension);
+    CppParamSet params;
+    std::string handle;
+    int32_t errcode;
+    EXPECT_EQ(hksCryptoExtStubImpl.ClearUkeyPinAuthState(handle, params, errcode), HKS_ERROR_EXT_UNDEFINED_OPERATION);
+    HksCryptoExtStubImpl hksCryptoExtStubImplNullptr(nullptr);
+    EXPECT_EQ(hksCryptoExtStubImplNullptr.ClearUkeyPinAuthState(handle, params, errcode), HKS_ERROR_EXT_NULLPTR);
 }
 }
