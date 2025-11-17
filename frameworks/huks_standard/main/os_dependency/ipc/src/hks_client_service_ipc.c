5b04e5ef2cf57869e1b7cdee8b933b694a8e42a2
zw20 <zouwei49@huawei.com>
2025-11-17 11:35:13 +0800

    update
    Signed-off-by: zw20 <zouwei49@huawei.com>

--- frameworks/huks_standard/main/os_dependency/ipc/src/hks_client_service_ipc.c
+++ frameworks/huks_standard/main/os_dependency/ipc/src/hks_client_service_ipc.c
@@ -367,7 +367,6 @@ int32_t HksClientAuthUkeyPin(const struct HksBlob *index, const struct HksParamS
             outBlob.data, sizeof(int32_t)), "memcpy_s outStatus failed")
         HKS_IF_NOT_EOK_LOGE_BREAK(memcpy_s(retryCount, sizeof(uint32_t),
             outBlob.data + sizeof(int32_t), sizeof(uint32_t)), "memcpy_s retryCount failed")
-
     } while (0);
 
     HksFreeParamSet(&newParamSet);
