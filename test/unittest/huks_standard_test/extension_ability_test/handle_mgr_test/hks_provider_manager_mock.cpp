9b7fd4b3723742ce55301f3c01973e4fcc3336e8
zw20 <zouwei49@huawei.com>
2025-11-22 10:39:22 +0800

    tdd update
    Signed-off-by: zw20 <zouwei49@huawei.com>

---
+++ test/unittest/huks_standard_test/extension_ability_test/handle_mgr_test/hks_provider_manager_mock.cpp
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2025-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hks_provider_life_cycle_manager.h"
+#include "hks_cpp_paramset.h"
+#include "hks_ukey_common.h"
+#include "hks_external_adapter.h"
+#include "hks_error_code.h"
+#include "hks_log.h"
+#include "hks_template.h"
+#include "huks_access_ext_base_stub.h"
+#include "iremote_broker.h"
+#include "iremote_object.h"
+#include "refbase.h"
+#include <memory>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <chrono>
+#include <thread>
+namespace OHOS::Security::Huks {
+
+class HksCryptoExtStubImpl : public HuksAccessExtBaseStub {
+public:
+    explicit HksCryptoExtStubImpl() = default;
+    ~HksCryptoExtStubImpl() {}
+
+    ErrCode OpenRemoteHandle(
+        const std::string& index,
+        const CppParamSet& params,
+        std::string& handle,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode CloseRemoteHandle(
+        const std::string& handle,
+        const CppParamSet& params,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode AuthUkeyPin(
+        const std::string& handle,
+        const CppParamSet& params,
+        int32_t& errcode,
+        int32_t& authState,
+        uint32_t& retryCnt) { return HKS_SUCCESS; };
+
+    ErrCode GetUkeyPinAuthState(
+        const std::string& handle,
+        const CppParamSet& params,
+        int32_t& state,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode Sign(
+        const std::string& handle,
+        const CppParamSet& params,
+        const std::vector<uint8_t>& inData,
+        std::vector<uint8_t>& outData,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode Verify(
+        const std::string& handle,
+        const CppParamSet& params,
+        const std::vector<uint8_t>& plainText,
+        const std::vector<uint8_t>& signature,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode ExportCertificate(
+        const std::string& index,
+        const CppParamSet& params,
+        std::string& certJsonArr,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode ExportProviderCertificates(
+        const CppParamSet& params,
+        std::string& certJsonArr,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode InitSession(
+        const std::string& index,
+        const CppParamSet& params,
+        std::string& handle,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode UpdateSession(
+        const std::string& handle,
+        const CppParamSet& params,
+        const std::vector<uint8_t>& inData,
+        std::vector<uint8_t>& outData,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode FinishSession(
+        const std::string& handle,
+        const CppParamSet& params,
+        const std::vector<uint8_t>& inData,
+        std::vector<uint8_t>& outData,
+        int32_t& errcode) { return HKS_SUCCESS; };
+
+    ErrCode GetProperty(
+        const std::string& handle,
+        const std::string& propertyId,
+        const CppParamSet& params,
+        CppParamSet& outParams,
+        int32_t& errcode) { return HKS_SUCCESS; };
+    
+    ErrCode GetResourceId(
+        const CppParamSet& params,
+        std::string& resourceId,
+        int32_t& errcode) { return HKS_SUCCESS; };
+    
+    ErrCode ClearUkeyPinAuthState(
+        const std::string& handle,
+        const CppParamSet& params,
+        int32_t& errcode) { return HKS_SUCCESS; };
+};
+
+bool ProviderInfo::operator==(const ProviderInfo &other) const
+{
+    return m_bundleName == other.m_bundleName && m_providerName == other.m_providerName &&
+        m_abilityName == other.m_abilityName;
+}
+
+bool ProviderInfo::operator<(const ProviderInfo &other) const
+{
+    return std::tie(m_bundleName, m_providerName, m_abilityName) <
+        std::tie(other.m_bundleName, other.m_providerName, other.m_abilityName);
+}
+
+std::shared_ptr<HksProviderLifeCycleManager> HksProviderLifeCycleManager::GetInstanceWrapper()
+{
+    return HksProviderLifeCycleManager::GetInstance();
+}
+
+void HksProviderLifeCycleManager::ReleaseInstance()
+{
+    HksProviderLifeCycleManager::DestroyInstance();
+}
+void HksProviderLifeCycleManager::PrintRegisterProviders()
+{
+}
+
+int32_t HksProviderLifeCycleManager::OnRegisterProvider(const HksProcessInfo &processInfo,
+    const std::string &providerName, const CppParamSet &paramSet, std::function<void(bool)> callback)
+{
+    return HKS_SUCCESS;
+}
+
+int32_t HksProviderLifeCycleManager::GetExtensionProxy(const ProviderInfo &providerInfo,
+    sptr<IHuksAccessExtBase> &proxy)
+{
+    proxy = sptr<HuksAccessExtBaseStub>(new HksCryptoExtStubImpl());
+    return HKS_SUCCESS;
+}
+
+int32_t HksProviderLifeCycleManager::HapGetAllConnectInfoByProviderName(const std::string &bundleName,
+    const std::string &providerName,
+    std::vector<std::pair<ProviderInfo, std::shared_ptr<HksExtAbilityConnectInfo>>> &providerInfos)
+{
+    return HKS_SUCCESS;
+}
+
+int32_t HksProviderLifeCycleManager::GetAllProviderInfosByProviderName(const std::string &providerName,
+    std::vector<ProviderInfo> &providerInfos)
+{
+    return HKS_SUCCESS;
+}
+
+int32_t HksProviderLifeCycleManager::HksHapGetConnectInfos(const HksProcessInfo &processInfo,
+    const std::string &providerName, const CppParamSet &paramSet,
+    std::vector<std::pair<ProviderInfo, std::shared_ptr<HksExtAbilityConnectInfo>>> &connectionInfos)
+{
+    return HKS_SUCCESS;
+}
+
+int32_t HksProviderLifeCycleManager::OnUnRegisterProvider(const HksProcessInfo &processInfo,
+    const std::string &providerName, [[maybe_unused]] const CppParamSet &paramSet, bool isdeath)
+{
+    return HKS_SUCCESS;
+}
+
+int32_t HksGetProviderInfo(const HksProcessInfo &processInfo, const std::string &providerName,
+    const CppParamSet &paramSet, ProviderInfo &providerInfo)
+{
+    return HKS_SUCCESS;
+}
+
+}
