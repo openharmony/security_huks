2bdd4d316c489ad2b8f3fbe191799334fa87752c
zw20 <zouwei49@huawei.com>
2025-12-23 22:00:13 +0800

    ukey 问题闭环
    Signed-off-by: zw20 <zouwei49@huawei.com>

--- services/huks_standard/huks_service/extension/ukey/session_manger/src/hks_session_manger.cpp
+++ services/huks_standard/huks_service/extension/ukey/session_manger/src/hks_session_manger.cpp
@@ -253,7 +253,7 @@ std::vector<uint32_t> HksSessionManager::FindToRemoveHandle(const uint32_t &uid)
     return toRemove;
 }
 
-std::vector<uint32_t> HksSessionManager::FindToRemoveByIndex(const uint32_t &uid, const std::string &abilityName,
+std::vector<uint32_t> HksSessionManager::FindToRemoveHandle(const uint32_t &uid, const std::string &abilityName,
     const std::string &index)
 {
     std::vector<uint32_t> toRemove;
@@ -264,7 +264,7 @@ std::vector<uint32_t> HksSessionManager::FindToRemoveByIndex(const uint32_t &uid
     return toRemove;
 }
 
-std::vector<uint32_t> HksSessionManager::FindToRemoveHandle(const uint32_t &uid, const std::string &index)
+std::vector<uint32_t> HksSessionManager::FindToRemoveByIndex(const uint32_t &uid, const std::string &index)
 {
     std::vector<uint32_t> toRemove;
     m_handlers.Iterate([&](const uint32_t &handle, HandleInfo &handleInfo) {
@@ -284,10 +284,10 @@ bool HksSessionManager::HksClearHandle(const HksProcessInfo &processInfo, const 
                 "the abilityName is too long. size: %" LOG_PUBLIC "zu", abilityName.second.size())
             std::string abilityNameStr = std::string(abilityName.second.begin(), abilityName.second.end());
             HKS_LOG_I("HksClearHandle get abilityName: %" LOG_PUBLIC "s", abilityNameStr.c_str());
-            toRemove = FindToRemoveByIndex(processInfo.uidInt, abilityNameStr, index);
+            toRemove = FindToRemoveHandle(processInfo.uidInt, abilityNameStr, index);
             break;
         }
-        toRemove = FindToRemoveHandle(processInfo.uidInt, index);
+        toRemove = FindToRemoveByIndex(processInfo.uidInt, index);
     } while (false);
     ClearSessionHandleMap(toRemove);
     return true;
