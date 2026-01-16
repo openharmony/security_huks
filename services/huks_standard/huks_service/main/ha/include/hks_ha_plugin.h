a26d538225d1941c48b9dc269fbcd09df7826b7c
slayyyy <tuduohui@huawei.com>
2026-01-16 15:50:56 +0800

    闭源DFX框架SA侧实现
    Signed-off-by: slayyyy <tuduohui@huawei.com>
    Change-Id: I45d97d4c6b76a1e6bb3034479245126ffc351e01

--- services/huks_standard/huks_service/main/ha/include/hks_ha_plugin.h
+++ services/huks_standard/huks_service/main/ha/include/hks_ha_plugin.h
@@ -18,6 +18,7 @@
 
 #include "hks_ha_event_queue.h"
 #include "hks_event_info.h"
+#include "hks_event_types.h"
 #include "hks_type.h"
 #include "hks_log.h"
 #include "hks_mem.h"
@@ -34,26 +35,6 @@
 
 constexpr uint32_t MAX_CACHE_SIZE = 50;
 constexpr time_t MAX_CACHE_DURATION = 3600; // Unit: seconds
-
-typedef int32_t (*HksParamSetToEventInfo)(const struct HksParamSet *paramSet, struct HksEventInfo *keyInfo);
-
-typedef bool (*HksEventInfoNeedReport)(const struct HksEventInfo *eventInfo);
-
-typedef bool (*HksEventInfoIsEqual)(const struct HksEventInfo *info1, const struct HksEventInfo *info2);
-
-typedef void (*HksEventInfoAdd)(struct HksEventInfo *info, const struct HksEventInfo *entry);
-
-typedef int32_t (*HksEventInfoToMap)(const struct HksEventInfo *info, std::unordered_map<std::string,
-    std::string>& map);
-
-typedef struct {
-    uint32_t eventId;
-    HksParamSetToEventInfo eventInfoCreate;
-    HksEventInfoNeedReport needReport;
-    HksEventInfoIsEqual eventInfoEqual;
-    HksEventInfoAdd eventInfoAdd;
-    HksEventInfoToMap eventInfoToMap;
-} HksEventProcMap;
 
 typedef struct {
     struct HksEventCommonInfo common;
@@ -135,12 +116,24 @@ public:
 
     bool Enqueue(uint32_t eventId, struct HksParamSet *paramSet);
 
+    int32_t RegisterEventProc(const struct HksEventProcMap *procMap);
+
+    int32_t RegisterEventProcs(const struct HksEventProcMap *procMaps, uint32_t count);
+
+    int32_t UnregisterEventProc(uint32_t eventId);
+
+    void ClearAllEventProcs();
+    
+    uint32_t GetEventProcCount() const;
+
 private:
     HksEventQueue queue;
     std::thread workerThread;
     std::atomic<bool> stopFlag;
     HksEventCacheList eventCacheList;
-    std::vector<HksEventProcMap *> eventProcMap;
+
+    std::vector<HksEventProcMap> eventProcList;
+    mutable std::mutex eventProcMutex;
 
     void WorkerThread();
 
@@ -161,6 +154,10 @@ private:
     HksEventProcMap* HksEventProcFind(uint32_t eventId);
 
     void HandlerReport(HksEventQueueItem &item);
+
+    void InitDefaultEventProcs();
+
+    bool IsValidEventProcMap(const struct HksEventProcMap *procMap) const;
 };
 
 #ifdef __cplusplus
@@ -170,6 +167,15 @@ extern "C" {
 int32_t HksHaPluginInit(void);
 
 void HksHaPluginDestroy(HksHaPlugin *plugin);
+
+// 供HksBasicInterface调用
+int32_t HksRegisterEventProcWrapper(const struct HksEventProcMap *procMap);
+
+int32_t HksRegisterEventProcs(const struct HksEventProcMap *procMaps, uint32_t count);
+
+int32_t HksUnregisterEventProcWrapper(uint32_t eventId);
+
+int32_t HksEnqueueEventWrapper(uint32_t eventId, struct HksParamSet *paramSet);
 
 #ifdef __cplusplus
 }
