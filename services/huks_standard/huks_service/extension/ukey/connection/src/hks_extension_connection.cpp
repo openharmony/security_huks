4fd74b8e1b61ca66749552d9ad204215059151a6
zhuke <zhuke27@huawei.com>
2026-01-29 15:38:56 +0800

    add
    Signed-off-by: zhuke <zhuke27@huawei.com>

--- services/huks_standard/huks_service/extension/ukey/connection/src/hks_extension_connection.cpp
+++ services/huks_standard/huks_service/extension/ukey/connection/src/hks_extension_connection.cpp
@@ -100,9 +100,8 @@ void ExtensionConnection::AddExtDeathRecipient(const wptr<IRemoteObject>& token)
     }
 
     if (callerDeathRecipient_ == nullptr) {
-        std::shared_ptr<ExtensionConnection> thisPtr = shared_from_this();
         callerDeathRecipient_ = new ExtensionDeathRecipient(std::bind(&ExtensionConnection::OnRemoteDied,
-            thisPtr, std::placeholders::_1));
+            this, std::placeholders::_1));
     }
 
     if (token != nullptr) {
