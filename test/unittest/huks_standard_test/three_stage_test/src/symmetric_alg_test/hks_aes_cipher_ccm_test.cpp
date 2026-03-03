8f99454040635cb210640b7a69200d8cf524b9c6
zhuke <zhuke27@huawei.com>
2026-03-03 16:41:40 +0800

    modif errorode
    Signed-off-by: zhuke <zhuke27@huawei.com>

--- test/unittest/huks_standard_test/three_stage_test/src/symmetric_alg_test/hks_aes_cipher_ccm_test.cpp
+++ test/unittest/huks_standard_test/three_stage_test/src/symmetric_alg_test/hks_aes_cipher_ccm_test.cpp
@@ -1298,14 +1298,14 @@ HWTEST_F(HksAesCipherCcmTest, HksAesCipherCcmTest013, TestSize.Level0)
 #ifdef HKS_UNTRUSTED_RUNNING_ENV
     EXPECT_EQ(ret, HKS_ERROR_CRYPTO_ENGINE_ERROR) << "not HKS_ERROR_CRYPTO_ENGINE_ERROR error code.";
 #else
-    EXPECT_EQ(ret, HKS_ERROR_INVALID_ARGUMENT) << "not HKS_ERROR_INVALID_ARGUMENT error code.";
+    EXPECT_EQ(ret, HKS_FAILURE) << "not HKS_FAILURE error code.";
 #endif
     inData.size = CCM_MAX_DATA_LEN_FOR_MAX_NONCE + AEAD_SIZE + 1;
     ret = HksAesCcmCipherTestDecrypt(&keyAlias, decParamSet, &inData, &cipherText, &inData, AEAD_SIZE);
 #ifdef HKS_UNTRUSTED_RUNNING_ENV
     EXPECT_EQ(ret, HKS_ERROR_CRYPTO_ENGINE_ERROR) << "not HKS_ERROR_CRYPTO_ENGINE_ERROR error code.";
 #else
-    EXPECT_EQ(ret, HKS_ERROR_INVALID_ARGUMENT) << "not HKS_ERROR_INVALID_ARGUMENT error code.";
+    EXPECT_EQ(ret, HKS_FAILURE) << "not HKS_FAILURE error code.";
 #endif
     
     EXPECT_EQ(HksDeleteKeyForDe(&keyAlias, genParamSet), HKS_SUCCESS) << "DeleteKey failed.";
