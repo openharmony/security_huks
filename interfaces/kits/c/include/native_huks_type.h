9fdd57b1da540e8dea0e01f6cdb8578286e286d5
lcy_lovebug <lichengyi5@h-partners.com>
2026-03-13 15:13:14 +0800

    修改C接口仓头文件syscap
    Signed-off-by: lcy_lovebug <lichengyi5@h-partners.com>

--- interfaces/kits/c/include/native_huks_type.h
+++ interfaces/kits/c/include/native_huks_type.h
@@ -20,7 +20,7 @@
  * @brief Defines the macros, enumerated values, data structures,
  *    and error codes used by OpenHarmony Universal KeyStore (HUKS) APIs.
  *
- * @syscap SystemCapability.Security.Huks
+ * @syscap SystemCapability.Security.Huks.Core
  * @since 9
  * @version 1.0
  */
