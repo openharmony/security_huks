c370031f6bddb8f554d7eabe9ed66086ad0a22b5
zhuke0413 <2739214278@qq.com>
2026-05-07 17:40:23 +0800

    add
    Signed-off-by: zhuke0413 <2739214278@qq.com>

--- services/huks_standard/huks_service/extension/ukey/handle_manager/src/hks_remote_handle_manager.cpp
+++ services/huks_standard/huks_service/extension/ukey/handle_manager/src/hks_remote_handle_manager.cpp
@@ -78,6 +78,16 @@ static int32_t WrapIndexWithProviderInfo(const ProviderInfo& providerInfo, const
     return HKS_SUCCESS;
 }
 
+static int32_t CheckIndexSizeAndSetIndex(const std::string& index, const ProviderInfo& providerInfo,
+    std::string& resourceId)
+{
+    HKS_IF_TRUE_LOGE_RETURN(index.size() > MAX_INDEX_SIZE, HKS_ERROR_NEW_INVALID_ARGUMENT,
+        "index is too long, size = %" LOG_PUBLIC "zu", index.size())
+    int32_t ret = WrapIndexWithProviderInfo(providerInfo, index, resourceId);
+    HKS_IF_NOT_SUCC_LOGE_RETURN(ret, ret, "WrapIndexWithProviderInfo fail, ret = %" LOG_PUBLIC "d", ret)
+    return HKS_SUCCESS;
+}
+
 static void ConvertCertInfoToIdl(const struct HksExtCertInfo &certInfo, HksExtCertInfoIdl &idlOut)
 {
     idlOut.purpose = certInfo.purpose;
@@ -590,8 +600,10 @@ int32_t HksRemoteHandleManager::GetResourceId(const HksProcessInfo &processInfo,
     
     ret = ConvertExtensionToHksErrorCode(ret, g_getResourceIdErrCodeMapping);
     HKS_IF_NOT_SUCC_LOGE_RETURN(ret, ret, "GetResourceId failed, ret = %" LOG_PUBLIC "d", ret)
-    
-    resourceId = resourceResult;
+
+    ret = CheckIndexSizeAndSetIndex(resourceResult, providerInfo, resourceId);
+    HKS_IF_NOT_SUCC_LOGE_RETURN(ret, ret , "CheckIndexSizeAndSetIndex fail, ret = %" LOG_PUBLIC "d", ret)
+
     HKS_LOG_I("leave HksRemoteHandleManager::GetResourceId, ret = %" LOG_PUBLIC "d", ret);
     return HKS_SUCCESS;
 }
