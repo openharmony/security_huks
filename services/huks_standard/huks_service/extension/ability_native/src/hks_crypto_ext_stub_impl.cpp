3285569cc5d894cc96fe4ce5bc120decc4883ee0
zhuke0413 <2739214278@qq.com>
2026-05-29 21:54:07 +0800

    add
    Signed-off-by: zhuke0413 <2739214278@qq.com>

--- services/huks_standard/huks_service/extension/ability_native/src/hks_crypto_ext_stub_impl.cpp
+++ services/huks_standard/huks_service/extension/ability_native/src/hks_crypto_ext_stub_impl.cpp
@@ -34,7 +34,7 @@ static void SetErrorInfoFromC(struct HksExternalErrorInfo *errInfoC, int32_t ret
     errorInfo.errorDesc.assign("");
 }
 
-int32_t HksCryptoExtStubImpl::HksExtStubCheckExtension(HksExternalErrorInfoIdl *errorInfo)
+int32_t HksCryptoExtStubImpl::HksExtStubCheckExtension(HksExternalErrorInfoIdl &errorInfo)
 {
     if (extension_ == nullptr) {
         LOGE("extension is nullptr");
@@ -45,7 +45,7 @@ int32_t HksCryptoExtStubImpl::HksExtStubCheckExtension(HksExternalErrorInfoIdl *
     return HKS_SUCCESS;
 }
 
-void HksCryptoExtStubImpl::HksExtStubInitErrorInfo(struct HksExternalErrorInfo &errInfoC)
+void HksCryptoExtStubImpl::HksExtStubInitErrorInfo(struct HksExternalErrorInfo *errInfoC)
 {
     errInfoC = HksCreateExternalErrorInfo(HKS_ERROR_EXT_JS_METHOD_ERROR, "");
     if (errInfoC == nullptr) {
