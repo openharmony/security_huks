798af15cbe85d70a695eb4bcbfe1dd2ff701a054
slayyyy <tuduohui@huawei.com>
2026-08-19 16:08:50 +0800

    update: 更新文件 hks_secure_access.c
    Signed-off-by: slayyyy <tuduohui@huawei.com>

--- services/huks_standard/huks_engine/main/core/src/hks_secure_access.c
+++ services/huks_standard/huks_engine/main/core/src/hks_secure_access.c
@@ -965,7 +965,7 @@ static int32_t HksCheckAuthType(struct HuksKeyNode *keyNode, const struct HksUse
     HKS_IF_TRUE_RETURN(ret == HKS_SUCCESS, HKS_SUCCESS);
 
     uint32_t blobAuthMode;
-    int32_t ret = GetAuthMode(keyNode->keyBlobParamSet, &blobAuthMode);
+    ret = GetAuthMode(keyNode->keyBlobParamSet, &blobAuthMode);
     HKS_IF_NOT_SUCC_LOGE_RETURN(ret, ret, "get auth mode failed")
     
     enum {
